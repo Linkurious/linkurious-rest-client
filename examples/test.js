6e879cc42852c1d3a2ca267517dc416ec870fd0f
maxime-allex <ma.allex@gmail.com>
2016-04-27 14:58:29 +0200

    add examples folder

---
+++ examples/test.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const Linkurious = require('./../built/index');
+
+const linkurious = new Linkurious('http://localhost:3000', 'debug');
+
+linkurious.login('ma.allex@gmail.com', 'grumaux');
+
+
