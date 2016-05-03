57a881426a284425f278532a71026a25237e580f
maxime-allex <ma.allex@gmail.com>
2016-05-03 16:06:18 +0200

    Add utils file

---
+++ src/utils.ts
@@ -0,0 +1,28 @@
+/**
+ * LINKURIOUS CONFIDENTIAL
+ * Copyright Linkurious SAS 2012 - 2016
+ *
+ * Created by maximeallex on 2016-04-28.
+ *
+ * File:
+ * Description :
+ */
+'use strict';
+
+export namespace Utils {
+
+  export function sanitizeQuery(data:any):any {
+    let result = <any>{};
+
+    for(let key in data){
+      if(data.hasOwnProperty(key)){
+        let sanitizedKey:string;
+
+        sanitizedKey = key.replace(/([A-Z])/g, (char) => '_' + char.toLowerCase());
+
+        result[sanitizedKey] = data[key];
+      }
+    }
+    return result;
+  }
+}
