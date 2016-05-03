ff532978e397d08fb7cda9b7f0364d56a82af113
maxime-allex <ma.allex@gmail.com>
2016-05-03 16:05:45 +0200

    implement AUTH APIs implement EDGES APIs implement GRAPH Apis implement LINKURIOUS APIs implement NODES APIs
    Add utilitary functions

--- src/index.ts
+++ src/index.ts
@@ -12,41 +12,563 @@
 /// <reference path="./../typings/main/ambient/node/index.d.ts" />
 /// <reference path="./../typings/main/definitions/request/index.d.ts" />
 
-import * as HTTPDriver from './HTTPDriver';
+import HTTPDriver from './HTTPDriver';
 import LogDriver from './logDriver';
-import {LinkuriousInterface, clientState, LoggerPlugin, LogDriverInterface} from './interfaces';
+import {Utils} from './utils';
+import * as Interface from './interfaces';
 
-class Linkurious implements LinkuriousInterface {
+class Linkurious implements Interface.LinkuriousInterface {
 
   public host:string;
-  public client:clientState;
-  public log:LogDriverInterface;
+  public state:Interface.clientState;
+  public log:Interface.LogDriverInterface;
+  public httpDriver:Interface.HTTPDriverInterface;
 
-  constructor(host:string, log:string, logger?:LoggerPlugin) {
-    this.host   = host + '/api/';
-    this.client = <clientState>{};
-
-    this.log = <LogDriverInterface>new LogDriver(log, logger);
+  constructor(host:string, log:string, logger?:Interface.LoggerPlugin) {
+    this.host       = host + '/api/';
+    this.state      = <Interface.clientState>{};
+    this.log        = <Interface.LogDriverInterface>new LogDriver(log, logger);
+    this.httpDriver = <Interface.HTTPDriverInterface>new HTTPDriver();
   }
 
-  public login(userLogin:string, password:string):Promise<any> {
+  /**
+   * test if condition is verified and return new currentSource
+   *
+   * @param source : StateSource
+   * @param comparator : string
+   * @param condition
+   * @returns {StateSource}
+   */
+  private setStateSource(source:Interface.Source, comparator:string, condition:any):any {
+  if (source[comparator] === condition) {
+    this.state.currentSource = {
+      name       : source.name,
+      key        : source.key,
+      configIndex: source.configIndex
+    };
+    return this.state.currentSource;
+  } else {
+    return false;
+  }
+}
 
+  /**
+   * construct the API URL with sourceId if needed and throw error if doesn't exists.
+   *
+   * @param uri:string
+   * @returns {string}
+   */
+  private transformUrl(uri:string):string {
+
+  if (uri.match(/^\{dataSource}$/)) {
+    if (this.state.currentSource.key) {
+      return this.state.currentSource.key + uri;
+    } else {
+      this.log.error({
+        key    : 'Configuration error',
+        message: 'You need to set a current source to fetch this API.'
+      });
+    }
+  } else {
+    return uri;
+  }
+}
+
+  /**
+   * HTTPDriver wrapper method
+   *
+   * @param method : string ('GET'|'POST'|'PATCH')
+   * @param uri : string
+   * @param data : object
+   * @returns {Promise}
+   */
+  private linkuriousFetch(method:string, uri:string, data?:any):Promise<any> {
+
+  let url = this.transformUrl(uri),
+      fetch;
+
+  if (!data) {
+    fetch = this.httpDriver[method](url);
+  } else {
+    fetch = this.httpDriver[method](url, data);
+  }
+
+  return fetch
+    .then((res) => res)
+    .catch((err) => {
+      this.log.error({
+        key    : err.status + ' - ' + err.type,
+        message: err.message
+      });
+
+      return Promise.reject(err);
+    });
+}
+
+  // ----------------------------------------------------- //
+  //                                                       //
+  //                 LINKURIOUS METHODS                    //
+  //                                                       //
+  // ----------------------------------------------------- //
+
+
+  /**
+   * Get the status of the all data-sources.
+   *
+   * @returns {Promise<SourcesList>}
+   */
+  public getSources():Promise<Interface.SourcesList> {
+    return this.linkuriousFetch('GET', 'dataSources');
+  }
+
+  /**
+   * Set the currentSource to the first source connected
+   *
+   * @returns {Promise<StateSource>}
+   */
+  public setDefaultSource():Promise<Interface.StateSource> {
+    return this.getSources()
+      .then((res) => {
+        res.sources = [res.sources[1], res.sources[0]];
+        for (let i = 0, l = res.sources.length; i < l; ++i) {
+          let sourceIteration = res.sources[i];
+
+          if (this.setStateSource(sourceIteration, 'connected', true)) {
+            return this.state.currentSource;
+          }
+        }
+      });
+  }
+
+  /**
+   * Set the currentSource by passing the sourceKey or configIndex
+   *
+   * @param keyOrConfig : string|number
+   * @returns {Promise<StateSource>}
+   */
+  public setCurrentSource(keyOrConfig:string | number):Promise<Interface.StateSource> {
+    return this.getSources()
+      .then((res) => {
+        for (let i = 0, l = res.sources.length; i < l; ++i) {
+          let sourceIteration = res.sources[i],
+              sourceComparator;
+
+          if (typeof keyOrConfig === 'string') {
+            sourceComparator = 'key';
+          } else {
+            sourceComparator = 'configIndex';
+          }
+
+          if (this.setStateSource.(sourceIteration, sourceComparator, keyOrConfig)) {
+            return this.state.currentSource;
+          }
+        }
+      });
+  }
+
+
+  // ----------------------------------------------------- //
+  //                                                       //
+  //                     AUTH METHODS                      //
+  //                                                       //
+  // ----------------------------------------------------- //
+
+
+  /**
+   * Process to login of the corresponding user and return it.
+   *
+   * @param userLogin : string
+   * @param password : string
+   * @returns {Promise<boolean>}
+   */
+  public userLogin(userLogin:string, password:string):Promise<boolean> {
     let data = {
       usernameOrEmail: userLogin,
       password       : password
     };
-
-    return HTTPDriver.POST(this.host + 'auth/login', data)
+    return this.linkuriousFetch('POST', 'auth/login', data)
       .then(res => {
-        this.client.user = res.user;
-        return new Promise((resolve:Function):Promise<boolean> => resolve(true));
+        this.state.user = res.user;
+        return true;
+      });
+  }
+
+  /**
+   * Check if the user is authenticated.
+   *
+   * @returns {Promise<boolean>}
+   */
+  public userIsAuth():Promise<boolean> {
+    return this.linkuriousFetch('GET', 'auth/authenticated')
+      .then(() => true)
+      .catch(() => false);
+  }
+
+  /**
+   * Check if the user is authenticated as an admin.
+   *
+   * @returns {Promise<boolean>}
+   */
+  public userIsAdmin():Promise<boolean> {
+    return this.linkuriousFetch('GET', 'auth/admin')
+      .then(() => true)
+      .catch(() => false);
+  }
+
+  /**
+   * Clear the user session.
+   *
+   * @returns {Promise<string>}
+   */
+  public logout():Promise<string> {
+    return this.linkuriousFetch('GET', 'auth/logout')
+      .then(() => 'user disconnected');
+  }
+
+  /**
+   * Retrieve the current user only if they are authenticated.
+   *
+   * @returns {Promise<User>}
+   */
+  public getCurrentUser():Promise<Interface.User> {
+    return this.linkuriousFetch('GET', 'auth/me');
+  }
+
+  /**
+   * Update the current user connected
+   *
+   * @param data : object
+   * @returns {Promise}
+   */
+  public updateCurrentUser(data:Interface.Form.user.update):Promise<any> {
+    return this.linkuriousFetch('PATCH', 'auth/me', data)
+      .then((res) => {
+        this.state.user = Object.assign(this.state.user, res);
+        return res;
       })
-      .catch(res => {
-        this.log.error({
-          key    : res.status + ' - ' + res.type,
-          message: res.message
-        });
-      })
+      .catch((err) => Promise.reject(err));
+  }
+
+
+  // ----------------------------------------------------- //
+  //                                                       //
+  //                    EDGES METHODS                      //
+  //                                                       //
+  // ----------------------------------------------------- //
+
+
+  /**
+   * return the number of edges in the graph.
+   *
+   * @returns {Promise}
+   */
+  public edgesCount():Promise<any> {
+    return this.linkuriousFetch('GET', '{dataSource}/graph/edges/count');
+  }
+
+  /**
+   * Add an edge in the graph.
+   *
+   * @param data : object
+   * @returns {Promise<Edge>}
+   */
+  public createEdge(data:Interface.Form.edge.create):Promise<Interface.Edge> {
+    return this.linkuriousFetch('POST', '{dataSource}/graph/edges', data);
+  }
+
+  /**
+   * Modify the properties of an edge in the graph by the given ones. Keeps the other properties of
+   * the edge unchanged.
+   *
+   * @param edgeId : number
+   * @param data : Form.edge.update
+   * @returns {Promise<Edge>}
+   */
+  public updateEdge(edgeId:number, data:Interface.Form.edge.update):Promise<Interface.Edge> {
+    return this.linkuriousFetch('PATCH', '{dataSource}/graph/edges/' + edgeId, data);
+  }
+
+  /**
+   * Delete a edge from the graph.
+   *
+   * @param edgeId : number
+   * @returns {Promise<string>}
+   */
+  public deleteEdge(edgeId:number):Promise<string> {
+    return this.linkuriousFetch('DELETE', '{dataSource}/graph/edges/' + edgeId)
+      .then(() => 'edge ' + edgeId + ' deleted');
+  }
+
+  /**
+   * Get the adjacent edges of a node from the graph.
+   * If source is provided, return outgoing edges only.
+   * Else if target is provided, return incoming edges only.
+   * Else if adjacent is provided, return all adjacent edges.
+   *
+   * @param data : object
+   * @returns {Promise<EdgesList>}
+   */
+  public getAdjacentEdges(data:Interface.Form.edge.getAdjacent):Promise<Interface.EdgesList> {
+    let query;
+
+    if (data.orientation === 'in') {
+      data['source'] = data.nodeId;
+    } else if (data.orientation === 'out') {
+      data['target'] = data.nodeId;
+    } else if (data.orientation === 'both') {
+      data['adjacent'] = data.nodeId;
+    }
+
+    delete data.nodeId;
+    delete data.orientation;
+
+    query = Utils.sanitizeQuery(data);
+
+    return this.linkuriousFetch('GET', '{dataSource}/graph/edges', query);
+  }
+
+  /**
+   * Get an edge of the graph.
+   *
+   * @param edgeId : number
+   * @returns {Promise<Edge>}
+   */
+  public getEdge(edgeId:number):Promise<Interface.Edge> {
+    return this.linkuriousFetch('GET', '{dataSource}/graph/edges/' + edgeId);
+  }
+
+
+  // ----------------------------------------------------- //
+  //                                                       //
+  //                    GRAPH METHODS                      //
+  //                                                       //
+  // ----------------------------------------------------- //
+
+
+  /**
+   * Delete a saved Graph Query owned by the current user
+   *
+   * @param graphQueryId : number
+   * @returns {Promise<string>}
+   */
+  public deleteGraphQuery(graphQueryId:number):Promise<string> {
+    return this.linkuriousFetch('DELETE', '{dataSource}/graph/my/rawQuery/' + graphQueryId)
+      .then(() => 'graph query ' + graphQueryId + ' deleted');
+  }
+
+  /**
+   * Returns a saved Graph Query owned by the current user
+   *
+   * @param graphQueryId : number
+   * @returns {Promise<GraphQuery>}
+   */
+  public getGraphQuery(graphQueryId:number):Promise<Interface.GraphQuery> {
+    return this.linkuriousFetch('GET', '{dataSource}/graph/my/rawQuery/' + graphQueryId);
+  }
+
+  /**
+   * Get the edit-versions for nodes and edges.
+   *
+   * @param nodesAndEdgesVersions : RequestNodesAndEdgesVersionsInterface
+   * @returns {Promise}
+   */
+  public getVersions(nodesAndEdgesVersions:Interface.RequestNodesAndEdgesVersionsInterface):Promise<any> {
+    return this.linkuriousFetch('POST', '{dataSource}/graph/versions', nodesAndEdgesVersions);
+  }
+
+  /**
+   * Returns an array of <LkNode[]> corresponding to all the shortest paths between two nodes that the user can read.
+   *
+   * @param nodesConfig : RequestShortestPathInterface
+   * @returns {Promise}
+   */
+  public getShortestPaths(nodesConfig:Interface.RequestShortestPathInterface):Promise<any> {
+    return this.linkuriousFetch('GET', '{dataSource}/graph/shortestPaths', nodesConfig);
+  }
+
+  /**
+   * Returns all saved Graph Queries owned by the current user
+   *
+   * @returns {Promise}
+   */
+  public getAllGraphQueries():Promise<any> {
+    return this.linkuriousFetch('GET', '{dataSource}/graph/my/rawQuery/all');
+  }
+
+  /**
+   * Save and Returns the created GraphQuery
+   * @param data : Form.rawQuery.create
+   * @returns {Promise<GraphQuery>}
+   */
+  public saveGraphQuery(data:Interface.Form.rawQuery.create):Promise<Interface.GraphQuery> {
+    return this.linkuriousFetch('POST', '{dataSource}/graph/my/rawQuery', data);
+  }
+
+  /**
+   * Returns an array of LkNode[] matching the sent query.
+   *
+   * @param data:RequestGraphWithQueryInterface
+   * @returns {Promise}
+   */
+  public requestGraphWithQuery(data:Interface.RequestGraphWithQueryInterface):Promise<any> {
+    return this.linkuriousFetch('POST', '{dataSource}/graph/rawQuery', data);
+  }
+
+  /**
+   * Update a graph query owned but the current user
+   * @param graphQueryId : number
+   * @param data : Form.rawQuery.update
+   * @returns {Promise<GraphQuery>}
+   */
+  public updateGraphQuery(graphQueryId:number, data:Interface.Form.rawQuery.update):Promise<Interface.GraphQuery> {
+    return this.linkuriousFetch('PATCH', '{dataSource}/graph/my/rawQuery' + graphQueryId, data);
+  }
+
+
+
+  // ----------------------------------------------------- //
+  //                                                       //
+  //                 LINKURIOUS METHODS                    //
+  //                                                       //
+  // ----------------------------------------------------- //
+
+
+  /**
+   * Find a list of users matching a filter (on username or email)
+   *
+   * @param data : Interface.Form.user.search
+   * @returns {Promise<Interface.SearchUserResult>}
+   */
+  public searchUsers(data:Interface.Form.user.search):Promise<Interface.SearchUserResult> {
+    return this.linkuriousFetch('GET', 'findUsers', data);
+  }
+
+  /**
+   * Get the status of the Linkurious API.
+   *
+   * @returns {Promise<Interface.AppStatus>}
+   */
+  public getAppStatus():Promise<Interface.AppStatus> {
+    return this.linkuriousFetch('GET', 'status');
+  }
+
+  /**
+   * Get Linkurious' current version information
+   *
+   * @returns {Promise<Interface.AppVersion>}
+   */
+  public getAppVersion():Promise<Interface.AppVersion> {
+    return this.linkuriousFetch('GET', 'version');
+  }
+
+  /**
+   * Return the configuration of the application.
+   *
+   * @param sourceIndex:number
+   * @returns {Promise<Interface.AppConfig>}
+   */
+  public getSourceConfig(sourceIndex?:number):Promise<Interface.AppConfig> {
+
+    let data = (sourceIndex)? sourceIndex : null;
+
+    return this.linkuriousFetch('GET', 'config', data);
+  }
+
+  /**
+   * Sets the configuration of the application
+   *
+   * @param data:Interface.Form.config.update
+   * @returns {Promise<string>}
+   */
+  public updateConfiguration(data : Interface.Form.config.update):Promise<string> {
+    return this.linkuriousFetch('POST', 'config', data);
+  }
+
+
+
+  // ----------------------------------------------------- //
+  //                                                       //
+  //                    NODES METHODS                      //
+  //                                                       //
+  // ----------------------------------------------------- //
+
+
+  /**
+   * Number of nodes in the graph.
+   *
+   * @returns {Promise<Interface.count>}
+   */
+  public countNodes():Promise<Interface.Count> {
+    return this.linkuriousFetch('GET', '{dataSource}/graph/nodes/count');
+  }
+
+  /**
+   * Add a node in the graph.
+   *
+   * @param data:Interface.Form.node.create
+   * @returns {Promise<Interface.Node>}
+   */
+  public createNode(data:Interface.Form.node.create):Promise<Interface.Node> {
+    return this.linkuriousFetch('POST', '{dataSource}/graph/nodes', data);
+  }
+
+  /**
+   * Delete the node and its adjacent edges from the graph.
+   *
+   * @param nodeId:number
+   * @returns {Promise<string>}
+   */
+  public deleteNode(nodeId:number):Promise<string> {
+    return this.linkuriousFetch('DELETE', '{dataSource}/graph/nodes/' + nodeId)
+      .then(() => 'node ' + nodeId + ' deleted');
+  }
+
+  /**
+   * Get a node from the graph.
+   *
+   * @param nodeId:number
+   * @param data:Interface.RequestNode
+   * @returns {Promise<Interface.Node>}
+   */
+  public getNode(nodeId:number, data?:Interface.RequestNode):Promise<Interface.Node> {
+    let params = (data)? data : null;
+
+    return this.linkuriousFetch('GET', '{dataSource}/graph/nodes/' + nodeId, params);
+  }
+
+  /**
+   * Get all adjacent nodes and edges for one or many source nodes (ids). The result is an array of
+   * nodes containing the sources nodes (ids) and their neighbors. Edges between sources nodes and
+   * neighbors - as well as edges between neighbors themselves - are returned in each node's edges
+   * field. If visible_nodes was specified, edges between source nodes or their neighbors and
+   * visible nodes are also included.
+   *
+   * @param data:Interface.RequestNodeAdjacentItems
+   * @returns {Promise<Interface.NodesWithEdges>}
+   */
+  public expandNode(data:Interface.RequestNodeAdjacentItems):Promise<Interface.NodesWithEdges> {
+    return this.linkuriousFetch('POST', '{dataSource}/graph/nodes/expand', data);
+  }
+
+  /**
+   * Get node-categories and edge-types of neighbors
+   *
+   * @param data:Interface.RequestNodeNeighbors
+   * @returns {Promise<Array<Interface.DigestItem>>}
+   */
+  public getNodeNeighbors(data:Interface.RequestNodeNeighbors):Promise<Array<Interface.DigestItem>> {
+    return this.linkuriousFetch('POST', '{dataSource}/graph/neighborhood/digest', data);
+  }
+
+  /**
+   * Modify the properties of a node in the graph by the given ones, and keeps the other properties of the node.
+   *
+   * @param nodeId:number
+   * @param data:Interface.Form.node.update
+   * @returns {Promise<Node>}
+   */
+  public updateNode(nodeId:number, data:Interface.Form.node.update):Promise<Node> {
+    return this.linkuriousFetch('PATCH', '{dataSource}/graph/nodes/' + nodeId, data);
   }
 }
 
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,7 +1,7 @@
 /*
-* user model
-*/
-export interface user {
+ * user model
+ */
+export interface User {
   id:number;
   username:string;
   email:string;
@@ -11,21 +11,55 @@ export interface user {
   actions:any;
 }
 
+export interface Group {
+  id : number;
+  name : string;
+  userCount : number;
+  builtIn : boolean;
+}
+
+export interface UserResult {
+  id : number;
+  username : string;
+  email : string;
+  ldap : boolean;
+  groups : Array<Group>
+}
+
+export interface SearchUserResult {
+  found : number;
+  results : Array<UserResult>;
+}
+
 /*
  * current source model
  */
-export interface currentSource {
+export interface StateSource {
   name:string;
   key:string;
   configIndex:number;
+}
+
+export interface Source {
+  name : string;
+  configIndex : number;
+  key : string;
+  connected : boolean;
+  state : string;
+  reason : string;
+  error : string;
+}
+
+export interface SourcesList {
+  sources : Array<Source>
 }
 
 /*
  * global REST client model
  */
 export interface clientState {
-  user:user;
-  currentSource:currentSource;
+  user:User;
+  currentSource:StateSource;
 }
 
 /*
@@ -33,8 +67,13 @@ export interface clientState {
  */
 export interface LinkuriousInterface {
   host:string;
-  client:clientState;
-  login(userLogin:string, password:string):Promise<any>;
+  state:clientState;
+  userLogin(userLogin:string, password:string):Promise<any>;
+  userIsAuth():Promise<boolean>;
+  userIsAdmin():Promise<boolean>;
+  logout():Promise<string>;
+  getCurrentUser(): Promise<User>;
+  updateCurrentUser(data:any) : Promise<any>;
 }
 
 /*
@@ -56,6 +95,16 @@ export interface LogDriverInterface {
 }
 
 /*
+ * HTTPDriver interface
+ */
+export interface HTTPDriverInterface {
+  POST(uri:string, data:any):Promise<any>;
+  PATCH(uri:string, data:any):Promise<any>;
+  GET(uri:string, data?:any):Promise<any>;
+  DELETE(uri:string):Promise<any>;
+}
+
+/*
  * server response body
  */
 export interface ResponseBody {
@@ -71,5 +120,251 @@ export interface ResponseBody {
 export interface ErrorBody {
   key:string;
   message:string;
+}
+
+export interface Edge {
+  id : number;
+  source : number;
+  target : number;
+  type : string;
+  data : any;
+}
+
+export interface GraphQuery {
+  content : string;
+  name : string;
+  dialect : string;
+  createdAt : string;
+  updatedAt : string;
+}
+
+export interface RequestNodesAndEdgesVersionsInterface {
+  nodes : Array<number>;
+  edges : Array<number>;
+}
+
+export interface RequestShortestPathInterface {
+  startNode : number;
+  endNode : number;
+  maxDepth ?: number;
+  with_version ?: boolean;
+}
+
+export interface RequestGraphWithQueryInterface {
+  dialect : string;
+  query : string;
+  with_version : boolean;
+}
+
+export interface AppStatus {
+  code : number;
+  name : string;
+  message : string;
+  uptime : number;
+}
+
+export interface AppVersion {
+  tag_name : string;
+  name : string;
+  prerelease : boolean;
+}
+
+export interface AllSourcesConfig {
+  maxPathLength : number;
+  shortestPathsMaxResults : number;
+  connectionRetries : number;
+  pollInterval : number;
+  indexationChunkSize : number;
+  expandThreshold : number;
+  searchAddAllThreshold : number;
+  searchThreshold : number;
+  minSearchQueryLength : number;
+  rawQueryTimeout : number;
+}
+
+export interface RightsConfig {
+  dataEdition : boolean;
+  loginTimeout : number;
+  widget : boolean;
+}
+
+export interface AnalyticsConfig {
+  enabled : boolean;
+  code : string;
+  domain : string;
+}
+
+export interface LeafletConfig {
+  name : string;
+  thumbnail : string;
+  urlTemplate : string;
+  attribution : string;
+  subdomains : string;
+  id : string;
+  accessToken : string;
+  minZoom : number;
+  maxZoom : number;
+}
+
+export interface AlternativeIds {
+  node : string;
+  edge : string;
+}
+
+export interface Directory {
+  nodes : boolean;
+  edges : boolean;
+}
+
+export interface SourceConfig {
+  features : any;
+  alternativeIds : AlternativeIds;
+  latitudeProperty : string;
+  longitudeProperty : string;
+  directory : Directory;
+}
+
+export interface AppConfig {
+  allSources : AllSourcesConfig;
+  rights : RightsConfig;
+  analytics : AnalyticsConfig;
+  sigma : any;
+  palette : any;
+  styles : any;
+  leaflet : LeafletConfig;
+  source : SourceConfig;
+  graphDb : any;
+  index : any;
+  enterprise : boolean;
+  domain : string;
+}
+
+export interface Count {
+  count : number;
+}
+
+export interface DigestItem {
+  edgeType : string;
+  nodeCategories : Array<string>;
+  nodes : number;
+  edges : number;
+}
+
+export interface DigestList {
+  digest : Array<DigestItem>;
+}
+
+export interface Node {
+  id : number;
+  data : any;
+  statistics ?: DigestList;
+  categories : any;
+}
+
+export interface NodesWithEdges extends Node{
+  edges : Array<Edge>;
+}
+
+export interface RequestNodeAdjacentItems {
+  ids : Array<number>;
+  ignored_nodes : Array<number>;
+  visible_nodes : Array<number>;
+  node_category : string;
+  edge_type : string;
+  limit : number;
+  limit_type : string;
+}
+
+export interface RequestNodeNeighbors {
+  ids : Array<number>;
+}
+
+export interface RequestNode {
+  with_edges ?: boolean;
+  with_digest ?: boolean;
+  with_version ?: boolean;
+}
+
+export type EdgesList = Array<Edge>;
+export type EdgeOrientation = 'in'|'out'|'both';
+
+export namespace Form {
+  export namespace user {
+    export interface update {
+      username ?: string;
+      email ?: string;
+      password ?:string;
+      preferences ?:any;
+    }
+
+    export interface search {
+      filter : string;
+      group_id : Array<number>;
+      unwanted_ids : Array<number>;
+      size : number;
+      start : number;
+    }
+  }
+
+  export namespace edge {
+    export interface create {
+      source : string;
+      target : string;
+      type : string;
+      data : any;
+    }
+
+    export interface update {
+      properties : any;
+      delete_properties : Array<string>;
+      type : string;
+    }
+
+    export interface getAdjacent {
+      orientation : EdgeOrientation;
+      nodeId : number;
+      type ?: string;
+      skip : number;
+      limit : number;
+      withVersion ?: boolean;
+    }
+  }
+
+  export namespace node {
+    export interface create {
+      properties ?: any;
+      categories ?: Array<string>;
+    }
+
+    export interface update {
+      properties :any;
+      deleted_properties : Array<string>;
+      added_properties : Array<string>;
+      deleted_categories : Array<string>;
+      version : number;
+    }
+  }
+
+  export namespace rawQuery {
+    export interface create {
+      dialect : string;
+      content : string;
+      name ?: string;
+    }
+
+    export interface update {
+      name : string;
+      content : string;
+    }
+  }
+
+  export namespace config {
+    export interface update {
+      key : string;
+      configuration : any;
+      sourceIndex ?: number;
+      reset ?: boolean;
+    }
+  }
 }
 
