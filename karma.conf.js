58bb49cbaafe758250e34e61c6962ca7db6fa933
maxime-allex <ma.allex@gmail.com>
2016-06-09 11:22:08 +0200

    #2 : add karma configuration file

---
+++ karma.conf.js
@@ -0,0 +1,95 @@
+// Karma configuration
+// Generated on Wed Jun 08 2016 15:30:52 GMT+0200 (CEST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '.',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['systemjs', 'jasmine'],
+
+    plugins : ['karma-systemjs', 'karma-jasmine', 'karma-chrome-launcher', 'karma-firefox-launcher', 'karma-safari-launcher'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'test/index_spec.ts'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {},
+
+    systemjs: {
+      configFile: 'system.conf.js',
+      config : {
+        baseURL : '.',
+        paths : {
+          "typescript" : "node_modules/typescript/lib/typescript.js",
+          "systemjs" : "node_modules/systemjs/dist/system.js",
+          "system-polyfills" : "node_modules/systemjs/dist/system-polyfills.js",
+          "es6-module-loader" : "node_modules/es6-module-loader/dist/es6-module-loader.js"
+        },
+        packages : {
+          'src' : {
+            defaultExtension : 'ts'
+          },
+          'test' : {
+            defaultExtension:'ts'
+          }
+        },
+        transpiler : 'typescript'
+      },
+      serveFiles: [
+        'node_modules/superagent/superagent.js',
+        'src/**/*.ts'
+      ]
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_DEBUG,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Safari'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+};
