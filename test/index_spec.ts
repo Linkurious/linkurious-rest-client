a4db36becbfc1901de929373256eb37683c1e33b
maxime-allex <ma.allex@gmail.com>
2016-06-09 11:18:55 +0200

    specs now in typescript

--- test/index_spec.ts
+++ test/index_spec.ts
@@ -2,24 +2,23 @@
  * LINKURIOUS CONFIDENTIAL
  * Copyright Linkurious SAS 2012 - 2016
  *
- * Created by maximeallex on 2016-05-10.
+ * Created by maximeallex on 2016-06-08.
  *
  * File:
  * Description :
  */
 'use strict';
 
-const should = require('should');
-const Linkurious = require('../built/index');
-const Logger = require('../built/log/Logger').Logger;
-const Fetcher = require('../built/http/fetcher').Fetcher;
-const AdminModule = require('../built/module/AdminModule').AdminModule;
+import {Linkurious} from '../src/index';
 
-describe('Linkurious class', function(){
 
-  let linkurious, testLog, logDriver, edgeID, nodeId, sourceKey;
 
-  beforeEach(function(){
+
+describe('Linkurious class', () => {
+
+  let linkurious:Linkurious, testLog:boolean, logDriver:any, edgeID:string|number, nodeId:string|number, sourceKey:string;
+
+  beforeEach(() => {
     testLog = false;
     logDriver = {
       debug: () => {},
@@ -28,127 +27,94 @@ describe('Linkurious class', function(){
       }
     };
     linkurious = new Linkurious('http://localhost:3001', 'error', logDriver);
-    sourceKey = global.sourceKey;
-  });
-
-  describe('constructor', function(){
-
-    it('log must be an instance of logDriver', function(){
-      linkurious._logger.should.be.an.instanceOf(Logger);
-    });
-
-    it('fetcher must be an instance of fetcher', function(){
-      linkurious._fetcher.should.be.an.instanceOf(Fetcher);
-    });
-
-    it('admin must be an instance of admin', function(){
-      linkurious.admin.should.be.an.instanceOf(AdminModule);
-    });
   });
 
   describe('searchNodes method', function(){
-    it('must return a node', function(){
+    it('must return a node', function(done){
       return linkurious.initCurrentSource().then(function(){
         return linkurious.search.nodes({
           q : 'Keanu Reeves'
         });
       }).then(function(res){
-        res.type.should.equal('node');
-        res.totalHits.should.equal(1);
-        res.results[0].title.should.equal('Person');
+        expect(res.type).toEqual('node');
+        expect(res.totalHits).toEqual(1);
+        expect(res.results[0].title).toEqual('Person');
 
         nodeId = res.results[0].children[0].id;
+        done();
       });
     });
 
-    it('must return an edge', function(){
+    it('must return an edge', function(done){
       return linkurious.initCurrentSource().then(function(){
         return linkurious.search.edges({
           q : 'ACTED_IN'
         });
       }).then(function(res){
-        res.type.should.equal('edge');
-        res.totalHits.should.equal(7);
+        expect(res.type).toEqual('edge');
+        expect(res.totalHits).toEqual(7);
         edgeID = res.results[0].children[0].id;
+        done();
       });
     });
-  });
-
-  describe('transformUrl method', function(){
-
-    it('must throw a console if no dataSource is set', function(){
-      linkurious._fetcher.transformUrl.bind(null, '/{dataSource}/api/test').should.throw()
-    });
-
-    it('must construct the url without sourceKey if no variable present in the url fragment', function(){
-      let url = linkurious._fetcher.transformUrl({
-        url : '/test'
-      });
-
-      url.should.equal('http://localhost:3001/api/test');
-    })
   });
 
   describe('initCurrentSource method', function(){
-    it('must set the right dataSource', function(){
+    it('must set the right dataSource', function(done){
       return linkurious.initCurrentSource().then(function(res){
-        // todo: detect the sourceKey in beforeAll (is not fixed)
-        res.should.eql({ name: 'Database #0', key: sourceKey, configIndex: 0 });
+        expect(res).toEqual({ name: 'Database #0', key: '66a2bc71', configIndex: 0 });
+        sourceKey = res.key;
+        done();
       });
     });
   });
 
   describe('setCurrentSource method', function(){
-    it('must set the dataSource by ConfigIndex', function(){
+    it('must set the dataSource by ConfigIndex', function(done){
       return linkurious.setCurrentSource(0).then(function(res){
-        res.should.eql({ name: 'Database #0', key: sourceKey, configIndex: 0 });
+        expect(res).toEqual({ name: 'Database #0', key: sourceKey, configIndex: 0 });
+        done();
       })
     });
 
-    it('must set the dataSource by key', function(){
+    it('must set the dataSource by key', function(done){
       return linkurious.setCurrentSource(sourceKey).then(function(res){
-        should(res).eql({name: 'Database #0', key: sourceKey, configIndex: 0});
+        expect(res).toEqual({name: 'Database #0', key: sourceKey, configIndex: 0});
+        done();
       })
     });
   });
-  
+
   describe('getAdjacentEdges method', function(){
-    it('must return correct value for in orientation', function(){
+    it('must return correct value for in orientation', function(done){
       return linkurious.initCurrentSource().then(function(){
-        return linkurious.edge.getAdjacentFromNode({
-          orientation:'in',
-          nodeId:nodeId,
-          withVersion :true
-        }).then(function(res){
-          res.should.have.length(3);
+        return linkurious.edge.getAdjacentFromNode({orientation:'in',nodeId:nodeId,withVersion:true}).then(function(res){
+          expect(res.length).toBe(3);
+          done();
         });
       });
     });
   });
 
   describe('getNodesByQuery method', function(){
-    it('must return the right nodes for the query', function(){
+    it('must return the right nodes for the query', function(done){
       return linkurious.initCurrentSource().then(function(){
-
         return linkurious.graph.getNodeList({
           dialect : 'cypher',
           query : 'MATCH (n)\nreturn n LIMIT 1',
           withVersion : true
         });
       }).then(function(res){
-        res[0].data.name.should.eql('Andy Wachowski');
+        expect(res[0].data.name).toEqual('Andy Wachowski');
+        done();
       })
     });
   });
 
   describe('searchUsers method', function(){
-    it('must return a list of users', function(){
-      return linkurious.search.users({
-        groupId:4,
-        size:20,
-        start:0
-      }).then(function(res){
-        res.should.eql({
+    it('must return a list of users', function(done){
+      return linkurious.search.users({groupId:[4], size:4, start:0}).then(function(res){
+        expect(res).toEqual({
           found: 1,
           results: [
             {
@@ -163,12 +129,13 @@ describe('Linkurious class', function(){
             }
           ]
         });
+        done();
       })
     })
   });
 
   describe('getNode method', function(){
-    it('must return the right node', function(){
+    it('must return the right node', function(done){
       return linkurious.initCurrentSource().then(function(){
         return linkurious.node.getOne({
           id:nodeId,
@@ -176,50 +143,33 @@ describe('Linkurious class', function(){
           withEdges : true
         });
       }).then(function(res){
-        res.id.should.equal(nodeId);
-        res.data.should.eql({ name: 'Keanu Reeves', born: 1964 });
-        res.edges.should.have.length(3);
+        expect(res.id).toEqual(nodeId);
+        expect(res.data).toEqual({ name: 'Keanu Reeves', born: 1964 });
+        expect(res.edges.length).toEqual(3);
+        done();
       });
     });
   });
 
   describe('expandNode method', function(){
-    it('must return the right array of nodes and edges', function(){
+    it('must return the right array of nodes and edges', function(done){
       return linkurious.initCurrentSource().then(function(){
         return linkurious.node.expand({
-          ids : [nodeId],
-          ignoredNodes : [],
-          visibleNodes : [nodeId],
-          withVersion : false
+          ids:[nodeId],
+          ignoredNodes:[],
+          visibleNodes:[nodeId],
+          withVersion:false
         });
       }).then(function(res){
-        res.should.have.length(4);
+        expect(res.length).toEqual(4);
+        done();
       });
     });
   });
 
   describe('updateNode method', function(){
-    it('must return an error object', function(){
-      return linkurious.initCurrentSource().then(function(){
-        return linkurious.node.update({
-          id:nodeId,
-          addedCategories : [],
-          deletedCategories : [],
-          deletedProperties : [],
-          properties : {name : 'Keanu Reeves', born : 1964, test:'test update'},
-          version : 1
-        });
-      }).catch(function(res){
-        res.should.eql({
-          key: 'edit_conflict',
-          message: 'Node edit conflict. (client version: 1, server version: 2)',
-          status: 409,
-          type: 'business'
-        })
-      })
-    });
 
-    it('must return true', function(){
+    it('must return true', function(done){
       return linkurious.initCurrentSource().then(function(){
         return linkurious.node.update({
           id:nodeId,
@@ -230,85 +180,90 @@ describe('Linkurious class', function(){
           version : 2
         });
       }).catch(function(res){
-        console.log(res);
-        res.should.be.true();
+        expect(res).toBeTruthy();
+        done();
       });
     });
   });
 
   describe('getEdgeProperties', function(){
-    it('must return a list of edges properties', function(){
+    it('must return a list of edges properties', function(done){
       return linkurious.initCurrentSource().then(function(){
         return linkurious.edge.getProperties({
           omitNoindex : true
         });
       }).then(function(res){
-        res.should.eql([ { key: 'altEdgeID', count: 1 }, { key: 'roles', count: 4 } ]);
+        expect(res).toEqual([ { key: 'altEdgeID', count: 1 }, { key: 'roles', count: 4 } ]);
+        done();
       });
     });
   });
 
   describe('getNodeProperties', function(){
-    it('must return a list of node properties', function(){
+    it('must return a list of node properties', function(done){
       return linkurious.initCurrentSource().then(function(){
         return linkurious.node.getProperties({
           omitNoindex : true
         });
       }).then(function(res){
-        res.should.have.length(6);
-        res[0].key.should.equal('born');
-        res[0].count.should.equal(8);
+        expect(res.length).toEqual(5);
+        expect(res[0].key).toEqual('born');
+        expect(res[0].count).toEqual(8);
+        done();
       });
     });
   });
 
   describe('getEdgeTypes method', function(){
-    it('must return edges types', function(){
+    it('must return edges types', function(done){
       return linkurious.initCurrentSource().then(function(){
         return linkurious.edge.getTypes({
           includeType : true
         });
       }).then(function(res){
-        res.should.eql([
+        expect(res).toEqual([
             { name: 'DIRECTED', count: 7, properties: [] },
             { name: 'IS_BRO_OF', count: 2, properties: [{count:1, key:'altEdgeID', type:'string'}] },
             { name: 'ACTED_IN', count: 7, properties: [{count:1, key:'edgeHiddenProp'}, {count:1, key:'edgeNoIndexProp'}, {count:4, key:'roles', type:'string'}] },
             { name: 'SON_OF', count: 1, properties: [] }
           ]
-        )
+        );
+        done();
       });
     });
   });
 
   describe('getNodeTypes method', function(){
-    it('must return node types', function(){
+    it('must return node types', function(done){
       return linkurious.initCurrentSource().then(function(){
         return linkurious.node.getTypes();
       }).then(function(res){
-        res.should.have.length(7);
-        res[0].name.should.equal('Person');
+        expect(res.length).toEqual(7);
+        expect(res[0].name).toEqual('Person');
+        done();
       });
     })
   });
 
   describe('getSandbox method', function(){
-    it('must return a visualization object', function(){
+    it('must return a visualization object', function(done){
       return linkurious.initCurrentSource().then(function(){
         return linkurious.visualization.getSandbox({
           doLayout : false
         });
       }).then(function(res){
-        res.visualization.should.have.property('id');
-        res.visualization.should.have.property('title', 'SandBox');
-        res.visualization.nodes.should.have.length(0);
-        res.visualization.edges.should.have.length(0);
-        res.visualization.should.have.property('layout', {});
+        expect(res.id).toBeTruthy();
+        expect(res.title).toBeTruthy();
+        expect(res.nodes.length).toEqual(0);
+        expect(res.edges.length).toEqual(0);
+        expect(res.layout).toBeTruthy();
+        done();
       });
     });
   });
 
   describe('shareVisualization method', function(){
-    it('must share a visualization', function(){
+    it('must share a visualization', function(done){
       return linkurious.initCurrentSource().then(function(){
         return linkurious.visualization.share({
           userId:3,
@@ -316,21 +271,22 @@ describe('Linkurious class', function(){
           vizId:4
         });
       }).then(function(res){
-        res.visualizationId.should.equal(4);
-        res.userId.should.equal(3);
+        expect(res.visualizationId).toEqual(4);
+        expect(res.userId).toEqual(3);
+        done();
       });
     });
   });
 
   describe('createUser method', function(){
-    it('must create a new user', function(){
+    it('must create a new user', function(done){
       return linkurious.admin.createUser({
         username:'testName',
         email:'testName@test.fr',
         groups : [4],
         password:'testPass'
       }).then(function(res){
-        res.should.eql({
+        expect(res).toEqual({
           id: 6,
           username: 'testName',
           email: 'testName@test.fr',
@@ -339,28 +295,30 @@ describe('Linkurious class', function(){
           admin: true,
           preferences: {}
         });
+        done();
       });
-    });
+    } );
   });
 
   describe('updateConfig method', function(){
     let test = false;
-    it('must change app config', function(){
+    it('must change app config', function(done){
       return linkurious.admin.updateConfig({
         path : 'access.authRequired',
         configuration : true
       }).then(function(){
         return test = true;
       }).then(function(res){
-        res.should.be.true();
+        expect(res).toBeTruthy();
+        done();
       });
     });
   });
 
   describe('login method', function(){
-  it('must log a user and hydrate app state', function(){
+    it('must log a user and hydrate app state', function(done){
       return linkurious.login({usernameOrEmail:'testName',password:'testPass'}).then(function(res){
-        linkurious.state.user.should.eql({
+        expect(linkurious.state.user).toEqual({
           id: 6,
           username: 'testName',
           email: 'testName@test.fr',
@@ -370,14 +328,15 @@ describe('Linkurious class', function(){
           preferences: {},
           actions: { all: [ 'rawReadQuery', 'rawWriteQuery' ] }
         });
-        res.should.be.true();
+        expect(res).toBeTruthy();
+        done();
       })
     });
 
-    it('must logout before login if another user is currently authenticated', function(){
+    it('must logout before login if another user is currently authenticated', function(done){
       return linkurious.login({usernameOrEmail:'testName',password:'testPass'}).then(function(){
         return linkurious.login({usernameOrEmail:'testName',password:'testPass'}).then(function(res){
-          linkurious.state.user.should.eql({
+          expect(linkurious.state.user).toEqual({
             id: 6,
             username: 'testName',
             email: 'testName@test.fr',
@@ -387,53 +346,57 @@ describe('Linkurious class', function(){
             preferences: {},
             actions: { all: [ 'rawReadQuery', 'rawWriteQuery' ] }
           });
-          res.should.be.true();
+          expect(res).toBeTruthy();
+          done();
         });
       })
     })
   });
 
   describe('logout method', function(){
-    it('must disconnect user and reset client state', function(){
+    it('must disconnect user and reset client state', function(done){
       return linkurious.login({usernameOrEmail:'testName',password:'testPass'}).then(function(){
         return linkurious.logout();
       }).then(function(res){
-        res.should.equal('user disconnected');
-        should(linkurious.state.user).not.ok();
+        expect(res).toEqual('user disconnected');
+        expect(linkurious.state.user).toBeUndefined();
+        done();
       })
     })
   });
 
   describe('startClient method', function(){
-    it('must set current user and default source', function(){
+    it('must set current user and default source', function(done){
       return linkurious.startClient({usernameOrEmail:'testName',password:'testPass'}).then(function(){
-        linkurious.state.user.should.eql({
-            id: 6,
-            username: 'testName',
-            email: 'testName@test.fr',
-            groups: [{'builtin':true, id : 4, name : 'admin'}],
-            ldap: false,
-            admin: true,
-            preferences: {},
-            actions: { all: ['rawReadQuery', 'rawWriteQuery'] }
+        expect(linkurious.state.user).toEqual({
+          id: 6,
+          username: 'testName',
+          email: 'testName@test.fr',
+          groups: [{'builtin':true, id : 4, name : 'admin'}],
+          ldap: false,
+          admin: true,
+          preferences: {},
+          actions: { all: ['rawReadQuery', 'rawWriteQuery'] }
         });
 
-        linkurious.state.currentSource.should.eql({ name: 'Database #0', key: global.sourceKey, configIndex: 0 });
+        expect(linkurious.state.currentSource).toEqual({ name: 'Database #0', key: sourceKey, configIndex: 0 });
+        done();
       });
     });
   });
 
   describe('updateCurrentUser', function(){
-    it('must update current user and reflect it in state', function(){
+    it('must update current user and reflect it in state', function(done){
       return linkurious.login({usernameOrEmail:'testName',password:'testPass'}).then(function(){
         return linkurious.updateCurrentUser({
+          id : 6,
           username : 'nameChanged'
         });
       }).then(function(){
-        linkurious.state.user.username.should.equal('nameChanged');
-        linkurious.state.user.id.should.equal(6);
-        linkurious.state.user.email.should.equal('testName@test.fr');
-        linkurious.state.user.should.eql({
+        expect(linkurious.state.user.username).toEqual('nameChanged');
+        expect(linkurious.state.user.id).toEqual(6);
+        expect(linkurious.state.user.email).toEqual('testName@test.fr');
+        expect(linkurious.state.user).toEqual({
           id: 6,
           username: 'nameChanged',
           email: 'testName@test.fr',
@@ -443,20 +406,23 @@ describe('Linkurious class', function(){
           preferences: {},
           actions: { all: ['rawReadQuery', 'rawWriteQuery'] }
         });
+        done();
       });
     });
   });
-  
+
   describe('processIndexation method', function(){
-    it('must start indexation and return true when finish', function(){
+    it('must start indexation and return true when finish', function(done){
       return linkurious.startClient({usernameOrEmail:'nameChanged',password:'testPass'}).then(function(){
         return linkurious.admin.processIndexation(50, function(res){
-          res.indexing.should.equal('ongoing');
+          expect(res.indexing).toEqual('ongoing');
         })
       }).then(function(res){
-        res.should.equal(true);
+        expect(res).toBeTruthy();
+        done();
       });
-    })
+    });
   })
 
 });
+
