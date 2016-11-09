6e1c2f8d288cb38a3db8f7f60a8b089abfefd344
davidrapin <david.rapin@gmail.com>
2016-11-09 10:40:59 +0100

    #19 fixed typings install issue

---
+++ scripts/installTypings.js
@@ -0,0 +1,15 @@
+/**
+ * LINKURIOUS CONFIDENTIAL
+ * Copyright Linkurious SAS 2012 - 2016
+ *
+ * - Created by david on 2016-11-09.
+ */
+'use strict';
+
+if ('production' === process.env['NODE_ENV']) {
+  console.log('Skipping "typings install" in production.');
+} else {
+  require('child_process').execSync(
+    'node ./node_modules/.bin/typings install', {cwd: require('path').resolve(__dirname, '..')}
+  );
+}
