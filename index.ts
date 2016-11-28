8c4d0d4bd81d3020b2e5a0a850a85663a50b2e77
maxime-allex <ma.allex@gmail.com>
2016-11-28 15:30:16 +0100

    WIP creating barrel for rest-client

--- index.ts
+++ index.ts
@@ -12,11 +12,21 @@
 import {Linkurious} from './src/index';
 import {Fetcher} from './src/http/fetcher';
 import {FetcherFactory} from './src/http/FetcherFactory';
-import * as IFetchConfig from './src/http/IFetchConfig';
+import {IDataSourceRelative, IDataToSend, IFetchConfig} from './src/http/IFetchConfig';
+import {IHttpResponse} from './src/http/IHttpResponse';
+import {Logger} from './src/log/Logger';
+import { ILoggerDriver } from './src/log/ILoggerDriver';
+let interfaces = require('./src/interfaces');
 
 export {
   Linkurious,
   Fetcher,
   FetcherFactory,
-  IFetchConfig
+  IFetchConfig,
+  IDataSourceRelative,
+  IDataToSend,
+  IHttpResponse,
+  interfaces,
+  Logger,
+  ILoggerDriver
 }
