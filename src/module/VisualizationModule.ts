5792bf488dc73f18a1748b787a43c8cbd0ec096e
maxime-allex <ma.allex@gmail.com>
2016-12-13 17:26:25 +0100

    [WIP] get visualization return ogma ready visualization

--- src/module/VisualizationModule.ts
+++ src/module/VisualizationModule.ts
@@ -49,8 +49,14 @@ export class VisualizationModule extends Module {
    */
   private static refactorItemsForOgma(items:Array<any>):void {
     items.map((item:any) => {
+      let data:any = JSON.parse(JSON.stringify(item.data));
       item.x = item.nodelink.x;
       item.y = item.nodelink.y;
+      item.data = {
+        properties : data,
+        categories : item.categories,
+        type : item.type
+      };
       delete item.nodelink;
       return item;
     });
