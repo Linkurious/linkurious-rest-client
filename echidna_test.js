ee663cc14952652c8ab8071e24aa96a3f9a09088
maxime-allex <ma.allex@gmail.com>
2017-01-31 13:04:51 +0100

    delete sudo

--- echidna_test.js
+++ echidna_test.js
@@ -24,9 +24,8 @@ module.exports = function(echidna) {
     exec('redir --lport=7484 --laddr=0.0.0.0 --cport=7484 ' +
       '--caddr=$(ip route show | awk \'/default/ {print $3}\') &');
 
-    exec('Xvfb :100 -ac &');
     // 5) Run grunt build
-    exec('sudo npm run test');
+    exec('npm run test');
   }).finally(() => {
     // 6) Stop Neo4j and elasticsearch
     exec('docker rm -vf neo4j_test_client es_test_client || true');
