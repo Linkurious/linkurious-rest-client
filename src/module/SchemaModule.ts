c4702c9dfa9bc68426ba5753815f67302f6710ae
Francesco Infante <francesco@linkurio.us>
2019-06-24 19:08:36 +0200

    Removed schema mocks

--- src/module/SchemaModule.ts
+++ src/module/SchemaModule.ts
@@ -12,7 +12,6 @@ import {Success} from '../response/success';
 import {Module} from './Module';
 
 import {
-  DataVisibility,
   ICreatePropertyParams,
   ICreatePropertyResponse,
   ICreateTypeParams,
@@ -27,54 +26,13 @@ import {
   IGetTypesResponse,
   IGetTypesWithAccessParams,
   IGetTypesWithAccessResponse,
-  IGraphSchemaProperty,
-  IGraphSchemaTypeWithAccess,
   ISetNonIndexedPropertiesParams,
   IStartSchemaSamplingParams,
   IStopSchemaSamplingParams,
   IUpdatePropertyParams,
   IUpdateSchemaSettingsParams,
-  IUpdateTypeParams,
-  LkPropertyType,
-  SamplingStatus
+  IUpdateTypeParams
 } from '../models/Schema';
-
-// TODO #76 remove Mock 2.8
-class Mock {
-  public static indexOf(
-    propertyKey: string,
-    items: Array<{propertyKey: string}>
-  ): number | undefined {
-    for (let i = 0; i < items.length; i++) {
-      if (items[i].propertyKey === propertyKey) {
-        return i;
-      }
-    }
-    return undefined;
-  }
-
-  public static property(
-    name: string,
-    p: ICreatePropertyParams | IUpdatePropertyParams
-  ): IGraphSchemaProperty {
-    // @ts-ignore
-    return {
-      propertyKey: p.propertyKey,
-      propertyType: p.propertyType || {name: LkPropertyType.AUTO},
-      required: p.required || false,
-      visibility: p.visibility || DataVisibility.SEARCHABLE
-    };
-  }
-
-  public static type(label: string, visibility?: DataVisibility): IGraphSchemaTypeWithAccess {
-    return {
-      access: 'writable',
-      label: label,
-      visibility: visibility || DataVisibility.SEARCHABLE,
-      properties: []
-    };
-  }
-}
 
 export class SchemaModule extends Module {
   public async startSchemaSampling(
@@ -116,12 +74,9 @@ export class SchemaModule extends Module {
     });
   }
 
-  private mockSchema: Map<string, IGraphSchemaTypeWithAccess> = new Map();
-
   public async createType(
     options: ICreateTypeParams
   ): Promise<Success<ICreateTypeResponse> | Unauthorized | Forbidden | DataSourceUnavailable> {
-    this.mockSchema.set(options.label, Mock.type(options.label, options.visibility));
     return this.request({
       url: '/admin/{sourceKey}/graph/schema/{entityType}/types',
       method: 'POST',
@@ -136,7 +91,6 @@ export class SchemaModule extends Module {
   public async updateType(
     options: IUpdateTypeParams
   ): Promise<Success<void> | Unauthorized | Forbidden | DataSourceUnavailable | NotFound> {
-    this.mockSchema.set(options.label, Mock.type(options.label, options.visibility));
     return this.request({
       url: '/admin/{sourceKey}/graph/schema/{entityType}/types',
       method: 'PATCH',
@@ -153,12 +107,6 @@ export class SchemaModule extends Module {
   ): Promise<
     Success<ICreatePropertyResponse> | Unauthorized | Forbidden | DataSourceUnavailable | NotFound
   > {
-    const mockValue = Mock.property(options.label, options);
-    const graphSchemaType = this.mockSchema.get(options.label);
-    if (graphSchemaType && !Mock.indexOf(options.propertyKey, graphSchemaType.properties)) {
-      graphSchemaType.properties.push(mockValue);
-    }
-
     return this.request({
       url: '/admin/{sourceKey}/graph/schema/{entityType}/properties',
       method: 'POST',
@@ -173,14 +121,6 @@ export class SchemaModule extends Module {
   public async updateProperty(
     options: IUpdatePropertyParams
   ): Promise<Success<void> | Unauthorized | Forbidden | DataSourceUnavailable | NotFound> {
-    const mockValue = Mock.property(options.label, options);
-    const graphSchemaType = this.mockSchema.get(options.label);
-    if (graphSchemaType) {
-      const property = Mock.indexOf(options.propertyKey, graphSchemaType.properties);
-      if (property) {
-        graphSchemaType.properties[property] = mockValue;
-      }
-    }
     return this.request({
       url: '/admin/{sourceKey}/graph/schema/{entityType}/properties',
       method: 'PATCH',
