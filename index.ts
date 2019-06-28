b64f84437d52518ba00d97843ef2d28ab9e64dbc
Francesco Infante <francesco@linkurio.us>
2019-06-28 16:38:11 +0200

    attempt to export

--- index.ts
+++ index.ts
@@ -5,6 +5,11 @@
  * - Created on 2016-11-28.
  */
 
+export * from './src/models/Alert';
+export * from './src/models/Graph';
+export * from './src/models/Schema';
+export * from './src/models/Visualization';
+
 import {
   BooleanTemplate,
   DateTemplate,
