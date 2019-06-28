e56b45e6b1fecc37940a11565b2d5cd51dd4e36c
Francesco Infante <francesco@linkurio.us>
2019-06-28 15:00:44 +0200

    Added interfaces rest client for create/update node and edges

--- src/models/Graph.ts
+++ src/models/Graph.ts
@@ -2,44 +2,50 @@
  * LINKURIOUS CONFIDENTIAL
  * Copyright Linkurious SAS 2012 - 2019
  *
- * - Created on 2019-05-20.
+ * - Created on 2019-06-28.
  */
 
-// type SchemaCompliantValue =
-//   | string
-//   | number
-//   | boolean
-//   | LkDate
-//   | LkDatetime
-//   | MissingValue
-//   | InvalidValue
-//   | ConflictValue;
-//
-// type LkPropertyValue = string | number | boolean | string[] | null;
+// TS2019-DONE
 
-// interface LkDate {
-//   type: LkPropertyType.DATE;
-//   value: number;
-// }
-//
-// interface LkDatetime {
-//   type: LkPropertyType.DATETIME;
-//   value: number;
-// }
-//
-// interface MissingValue {
-//   type: LkPropertyType;
-//   status: 'missing';
-// }
-//
-// interface InvalidValue {
-//   type: LkPropertyType;
-//   status: 'invalid';
-//   original: string; // when not of the good type we return a string representation (string[] feel in this category)
-// }
-//
-// interface ConflictValue {
-//   type: 'auto';
-//   status: 'conflict';
-//   original: string; // when schema is in conflict we return a string representation
-// }
+import { IDataSourceParams } from './Model';
+
+export interface ILkDate {
+  type: 'date';
+  value: string; // iso string UTC+0
+}
+
+export interface ILkDateTime {
+  type: 'datetime';
+  value: string; // iso string UTC+0
+}
+
+export interface ILkProperties {
+  [key: string]: string | number | boolean | ILkDate | ILkDateTime;
+}
+
+export interface ICreateNodeParams extends IDataSourceParams {
+  properties: ILkProperties;
+  categories: string[];
+}
+
+export interface IUpdateNodeParams extends IDataSourceParams {
+  id: number;
+  properties?: ILkProperties;
+  deletedProperties?: string[];
+  addedCategories?: string[];
+  deletedCategories?: string[];
+  readAt?: number;
+}
+
+export interface ICreateEdgeParams extends IDataSourceParams {
+  properties: ILkProperties;
+  type: string;
+  source: string;
+  target: string;
+}
+
+export interface IUpdateEdgeParams extends IDataSourceParams {
+  id: number;
+  properties?: ILkProperties;
+  deletedProperties?: string[];
+}
