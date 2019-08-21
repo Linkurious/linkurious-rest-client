7995cb9791de5fed00cf17644c5e5a8eea27f7a6
Jose Renato Ramos Gonzalez <renato@linkurio.us>
2019-08-21 10:44:58 +0200

    ran npm run pretty

--- src/models/CustomAction.ts
+++ src/models/CustomAction.ts
@@ -9,77 +9,79 @@
 
 // Create
 export interface ICreateCustomActionParams {
-    sourceKey: string;
-    name: string;
-    urlTemplate: string;
-    description: string;
-    sharing: CustomActionSharing
+  sourceKey: string;
+  name: string;
+  urlTemplate: string;
+  description: string;
+  sharing: CustomActionSharing;
 }
 export interface ICreateCustomActionResponse extends ICustomAction {}
 
 // Delete
 export interface IDeleteCustomActionParams {
-    sourceKey: string;
-    id: number;
+  sourceKey: string;
+  id: number;
 }
 
 // Update
 export interface IUpdateCustomActionParams {
-    sourceKey: string;
-    id: number;
-    name?: string;
-    urlTemplate?: string;
-    description?: string;
+  sourceKey: string;
+  id: number;
+  name?: string;
+  urlTemplate?: string;
+  description?: string;
 }
 export interface IUpdateCustomActionResponse extends ICustomAction {}
 
 // Get
 export interface IGetCustomActionsParams {
-    sourceKey: string;
+  sourceKey: string;
 }
-export type IGetCustomActionsResponse = ICustomAction[]
+export type IGetCustomActionsResponse = ICustomAction[];
 
 // Entities
 export interface ICustomAction {
-    sourceKey: string;
-    id: number;
-    name: string;
-    urlTemplate: string;
-    description: string;
-    sharing: CustomActionSharing;
-    type: CustomActionType;
-    elements: CustomActionElement[];
+  sourceKey: string;
+  id: number;
+  name: string;
+  urlTemplate: string;
+  description: string;
+  sharing: CustomActionSharing;
+  type: CustomActionType;
+  elements: CustomActionElement[];
 }
 export enum CustomActionSharing {
-    PRIVATE = 'private',
-    SOURCE = 'source'
+  PRIVATE = 'private',
+  SOURCE = 'source'
 }
 export enum CustomActionType {
-    NONE = 'none',
-    NON_GRAPH = 'non-graph',
-    NODE = 'node',
-    EDGE = 'edge',
-    NODESET = 'nodeset',
-    EDGESET = 'edgeset'
+  NONE = 'none',
+  NON_GRAPH = 'non-graph',
+  NODE = 'node',
+  EDGE = 'edge',
+  NODESET = 'nodeset',
+  EDGESET = 'edgeset'
 }
 export type CustomActionElement = {
-    value: string;
-    start: number;
-    end: number;
+  value: string;
+  start: number;
+  end: number;
 } & (
-    {
-        type: 'ca-literal';
-    } | {
-        type: 'ca-expression';
-        variable: 'visualization' | 'sourceKey';
-    } | {
-        type: 'ca-expression';
-        variable: 'nodeset' | 'edgeset';
-        label?: string;
-    } | {
-        type: 'ca-expression',
-        variable: 'node' | 'edge'
-        label?: string;
-        property?: string;
+  | {
+      type: 'ca-literal';
     }
-)
+  | {
+      type: 'ca-expression';
+      variable: 'visualization' | 'sourceKey';
+    }
+  | {
+      type: 'ca-expression';
+      variable: 'nodeset' | 'edgeset';
+      label?: string;
+    }
+  | {
+      type: 'ca-expression';
+      variable: 'node' | 'edge';
+      label?: string;
+      property?: string;
+    });
--- src/module/CustomActionModule.ts
+++ src/module/CustomActionModule.ts
@@ -7,84 +7,92 @@
 
 // TS2019-DONE
 
-import {DataSourceUnavailable, Forbidden, InvalidParameter, NotFound, Unauthorized} from '../response/errors';
+import {
+  DataSourceUnavailable,
+  Forbidden,
+  InvalidParameter,
+  NotFound,
+  Unauthorized
+} from '../response/errors';
 import {Success} from '../response/success';
 import {Module} from './Module';
 
 import {
-    ICreateCustomActionParams,
-    ICreateCustomActionResponse,
-    IDeleteCustomActionParams,
-    IUpdateCustomActionParams,
-    IUpdateCustomActionResponse,
-    IGetCustomActionsParams,
-    IGetCustomActionsResponse
+  ICreateCustomActionParams,
+  ICreateCustomActionResponse,
+  IDeleteCustomActionParams,
+  IUpdateCustomActionParams,
+  IUpdateCustomActionResponse,
+  IGetCustomActionsParams,
+  IGetCustomActionsResponse
 } from '../models/CustomAction';
 
 export class CustomActionModule extends Module {
+  public async createCustomAction(
+    options: ICreateCustomActionParams
+  ): Promise<
+    | Success<ICreateCustomActionResponse>
+    | Unauthorized
+    | Forbidden
+    | DataSourceUnavailable
+    | InvalidParameter
+  > {
+    return this.request({
+      url: '/admin/{sourceKey}/customAction',
+      method: 'POST',
+      body: options,
+      path: {
+        sourceKey: options.sourceKey
+      }
+    });
+  }
 
-    public async createCustomAction(options: ICreateCustomActionParams):
-        Promise<
-            Success<ICreateCustomActionResponse> |
-            Unauthorized | Forbidden | DataSourceUnavailable| InvalidParameter
-        >
-    {
-        return this.request({
-            url: '/admin/{sourceKey}/customAction',
-            method: 'POST',
-            body: options,
-            path: {
-                sourceKey: options.sourceKey
-            }
-        });
-    }
+  public async deleteCustomAction(
+    options: IDeleteCustomActionParams
+  ): Promise<Success<void> | Unauthorized | Forbidden | DataSourceUnavailable | NotFound> {
+    return this.request({
+      url: '/admin/{sourceKey}/customAction/{id}',
+      method: 'DELETE',
+      body: options,
+      path: {
+        sourceKey: options.sourceKey,
+        id: options.id
+      }
+    });
+  }
 
-    public async deleteCustomAction(options: IDeleteCustomActionParams):
-        Promise<
-            Success<void> |
-            Unauthorized | Forbidden | DataSourceUnavailable | NotFound
-        >
-    {
-        return this.request({
-            url: '/admin/{sourceKey}/customAction/{id}',
-            method: 'DELETE',
-            body: options,
-            path: {
-                sourceKey: options.sourceKey,
-                id: options.id
-            }
-        });
-    }
+  public async updateCustomAction(
+    options: IUpdateCustomActionParams
+  ): Promise<
+    | Success<IUpdateCustomActionResponse>
+    | Unauthorized
+    | Forbidden
+    | DataSourceUnavailable
+    | NotFound
+    | InvalidParameter
+  > {
+    return this.request({
+      url: '/admin/{sourceKey}/customAction/{id}',
+      method: 'PATCH',
+      body: options,
+      path: {
+        sourceKey: options.sourceKey,
+        id: options.id
+      }
+    });
+  }
 
-    public async updateCustomAction(options: IUpdateCustomActionParams):
-        Promise<
-            Success<IUpdateCustomActionResponse> |
-            Unauthorized | Forbidden | DataSourceUnavailable | NotFound | InvalidParameter
-        >
-    {
-        return this.request({
-            url: '/admin/{sourceKey}/customAction/{id}',
-            method: 'PATCH',
-            body: options,
-            path: {
-                sourceKey: options.sourceKey,
-                id: options.id
-            }
-        });
-    }
-
-    public async getCustomActions(options: IGetCustomActionsParams):
-        Promise<
-            Success<IGetCustomActionsResponse> |
-            Unauthorized | Forbidden | DataSourceUnavailable
-        >
-    {
-        return this.request({
-            url: '/admin/{sourceKey}/customAction',
-            method: 'GET',
-            path: {
-                sourceKey: options.sourceKey
-            }
-        });
-    }
+  public async getCustomActions(
+    options: IGetCustomActionsParams
+  ): Promise<
+    Success<IGetCustomActionsResponse> | Unauthorized | Forbidden | DataSourceUnavailable
+  > {
+    return this.request({
+      url: '/admin/{sourceKey}/customAction',
+      method: 'GET',
+      path: {
+        sourceKey: options.sourceKey
+      }
+    });
+  }
 }
