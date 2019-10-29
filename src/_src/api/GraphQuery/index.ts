33c9d01c5a8d05b60c8be0c469b28a8c2136ab57
Cheick Diarra <cheickdiarra74@gmail.com>
2019-10-29 17:58:32 +0100

    migrate GraphQuery module

---
+++ src/_src/api/GraphQuery/index.ts
@@ -0,0 +1,234 @@
+/**
+ * LINKURIOUS CONFIDENTIAL
+ * Copyright Linkurious SAS 2012 - 2019
+ *
+ * - Created on 2019-10-29.
+ */
+import { Request } from '../../http/request';
+import { LkErrorKey } from '../../http/response';
+import {
+  IAlertPreviewParams, IAlertPreviewResponse,
+  ICheckGraphQueryParams, ICheckGraphQueryResponse, ICreateGraphQueryParams, IDeleteGraphQueryParams,
+  IGetGraphQueriesParams,
+  IGetGraphQueryParams,
+  IGraphQueryResponse, IRunGraphQueryByContentParams, IRunGraphQueryByIdParams,
+  IRunGraphQueryResponse, IUpdateGraphQueryParams
+} from './types';
+
+const {INVALID_PARAMETER, UNAUTHORIZED, FORBIDDEN, NOT_FOUND, DATA_SOURCE_UNAVAILABLE, GUEST_DISABLED, BAD_GRAPH_REQUEST, CONSTRAINT_VIOLATION, GRAPH_REQUEST_TIMEOUT, GRAPH_UNREACHABLE} = LkErrorKey;
+
+export class GraphQueryApi extends Request {
+  /**
+   * Returns a saved GraphModule Query owned by the current user.
+   */
+  public getGraphQuery(params: IGetGraphQueryParams){
+    return this
+      .handle(
+        INVALID_PARAMETER,
+        UNAUTHORIZED,
+        GUEST_DISABLED,
+        FORBIDDEN,
+        BAD_GRAPH_REQUEST,
+        CONSTRAINT_VIOLATION,
+        GRAPH_REQUEST_TIMEOUT,
+        DATA_SOURCE_UNAVAILABLE,
+        GRAPH_UNREACHABLE
+      )
+      .request<IGraphQueryResponse>({
+        url: '/:sourceKey/graph/query/:id',
+        method: 'GET',
+        params: params
+      })
+  }
+
+  /**
+   * Returns all saved GraphModule Queries owned by the current user.
+   */
+  public getAllGraphQueries(params: IGetGraphQueriesParams) {
+    return this
+      .handle(
+        INVALID_PARAMETER,
+        UNAUTHORIZED,
+        FORBIDDEN,
+        GUEST_DISABLED,
+        BAD_GRAPH_REQUEST,
+        CONSTRAINT_VIOLATION,
+        GRAPH_REQUEST_TIMEOUT,
+        DATA_SOURCE_UNAVAILABLE,
+        GRAPH_UNREACHABLE
+      )
+      .request<IGraphQueryResponse[]>({
+        url: '/:sourceKey/graph/query',
+        method: 'GET',
+        params: params,
+        query: {type: params.type}
+      }
+    );
+  }
+
+  /**IRunGraphQueryParams
+   * Run a static or template query
+   */
+  public run(params: IRunGraphQueryByContentParams) {
+    return this
+      .handle(
+        INVALID_PARAMETER,
+        UNAUTHORIZED,
+        FORBIDDEN,
+        BAD_GRAPH_REQUEST,
+        CONSTRAINT_VIOLATION,
+        GRAPH_REQUEST_TIMEOUT,
+        DATA_SOURCE_UNAVAILABLE,
+        GRAPH_UNREACHABLE
+      )
+      .request<IRunGraphQueryResponse>({
+        url: '/:sourceKey/graph/run/query',
+        method: 'POST',
+        params: params,
+        query: {
+          withDigest: params.withDigest,
+          withDegree: params.withDegree,
+          withAccess: params.withAccess
+        }
+      }
+    );
+  }
+
+  /**
+   * Run a static or template query.
+   */
+  public runById(params: IRunGraphQueryByIdParams) {
+    return this
+      .handle(
+        INVALID_PARAMETER,
+        UNAUTHORIZED,
+        FORBIDDEN,
+        GUEST_DISABLED,
+        BAD_GRAPH_REQUEST,
+        CONSTRAINT_VIOLATION,
+        GRAPH_REQUEST_TIMEOUT,
+        DATA_SOURCE_UNAVAILABLE,
+        GRAPH_UNREACHABLE
+      )
+      .request<IRunGraphQueryResponse>({
+        url: '/:sourceKey/graph/run/query/:id',
+        method: 'POST',
+        params: params,
+        query: {
+          withDegree: params.withDegree,
+          withAccess: params.withAccess,
+          withDigest: params.withDigest
+        }
+      }
+    );
+  }
+
+  /**
+   * Return resolve if the current query is valid.
+   */
+  public check(params: ICheckGraphQueryParams) {
+    return this
+      .handle(
+        INVALID_PARAMETER,
+        UNAUTHORIZED,
+        FORBIDDEN,
+        BAD_GRAPH_REQUEST,
+        CONSTRAINT_VIOLATION,
+        GRAPH_REQUEST_TIMEOUT,
+        DATA_SOURCE_UNAVAILABLE,
+        GRAPH_UNREACHABLE
+      )
+      .request<ICheckGraphQueryResponse>({
+        url: '/:sourceKey/graph/check/query',
+        method: 'POST',
+        params: params
+      }
+    );
+  }
+
+  /**
+   * Preview the result of a query.
+   */
+  public preview(params: IAlertPreviewParams) {
+    return this
+      .handle(
+        INVALID_PARAMETER,
+        UNAUTHORIZED,
+        FORBIDDEN,
+        GUEST_DISABLED,
+        BAD_GRAPH_REQUEST,
+        CONSTRAINT_VIOLATION,
+        GRAPH_REQUEST_TIMEOUT,
+        DATA_SOURCE_UNAVAILABLE,
+        GRAPH_UNREACHABLE
+      )
+      .request<IAlertPreviewResponse>({
+        url: '/:sourceKey/graph/alertPreview',
+        method: 'POST',
+        params: params
+      }
+    );
+  }
+
+  /**
+   * Save and Returns the created GraphQuery.
+   */
+  public saveGraphQuery(params: ICreateGraphQueryParams) {
+    return this
+      .handle(
+        INVALID_PARAMETER,
+        UNAUTHORIZED,
+        FORBIDDEN,
+        BAD_GRAPH_REQUEST,
+        CONSTRAINT_VIOLATION,
+        GRAPH_REQUEST_TIMEOUT,
+        DATA_SOURCE_UNAVAILABLE,
+        GRAPH_UNREACHABLE
+      )
+      .request<IGraphQueryResponse>({
+        url: '/:sourceKey/graph/query',
+        method: 'POST',
+        params: params
+      }
+    );
+  }
+
+  /**
+   * Update a graph query owned but the current user.
+   */
+  public updateGraphQuery(params: IUpdateGraphQueryParams) {
+    return this
+      .handle(
+        INVALID_PARAMETER,
+        UNAUTHORIZED,
+        FORBIDDEN,
+        NOT_FOUND,
+        DATA_SOURCE_UNAVAILABLE,
+      )
+      .request({
+        url: '/:sourceKey/graph/query/:id',
+        method: 'PATCH',
+        params: params
+      });
+  }
+
+  /**
+   * Delete a query.
+   */
+  public delete(params: IDeleteGraphQueryParams) {
+    return this
+      .handle(
+        INVALID_PARAMETER,
+        UNAUTHORIZED,
+        FORBIDDEN,
+        NOT_FOUND,
+        DATA_SOURCE_UNAVAILABLE,
+      )
+      .request({
+        url: '/:sourceKey/graph/query/:id',
+        method: 'DELETE',
+        params: params
+      }
+    );
+  }
+}
---
+++ src/_src/api/GraphQuery/types.ts
@@ -0,0 +1,245 @@
+/**
+ * LINKURIOUS CONFIDENTIAL
+ * Copyright Linkurious SAS 2012 - 2019
+ *
+ * - Created on 2019-10-29.
+ */
+import { LkEdge, LkNode } from '../..';
+
+import { GenericObject, IDataSourceParams } from '../commonTypes';
+
+
+export interface TemplateFieldOptions {
+  [k: string]: string | number | boolean | string[] | object | undefined;
+}
+
+export enum TemplateFieldType {
+  NODE = 'node',
+  NODE_SET = 'nodeset',
+  STRING = 'string',
+  NUMBER = 'number',
+  BOOLEAN = 'boolean',
+  ENUM = 'enum',
+  DATE = 'date',
+  DATE_TIME = 'datetime'
+}
+
+export interface TemplateField<T extends TemplateFieldType = TemplateFieldType> {
+  key: string;
+  type: T;
+  options?: TemplateFieldOptions;
+}
+
+export type TemplateFields = Array<NumberTemplate | StringTemplate
+  | EnumTemplate | NodeTemplate | NodesetTemplate | DateTemplate
+  | DatetimeTemplate | BooleanTemplate>
+
+export type EnumValue = string | number | boolean;
+
+export type EnumChoices = Array<{
+  label: string;
+  value: EnumValue;
+}>;
+
+export enum DatetimeFormat {
+  TIMESTAMP = 'timestamp',
+  TIMESTAMP_MS = 'timestamp-ms',
+  ISO = 'iso',
+  YYYY_MM_DD_T = 'yyyy-MM-ddThh:mm:ss',
+  NATIVE = 'native'
+}
+
+export enum DateFormat {
+  TIMESTAMP = 'timestamp',
+  TIMESTAMP_MS = 'timestamp-ms',
+  ISO = 'iso',
+  ISO_YYYY_MM_DD = 'yyyy-MM-dd',
+  DD_MM_YYYY = 'dd/mm/yyyy',
+  MM_DD_YYYY = 'mm/dd/yyyy',
+  NATIVE = 'native'
+}
+
+export interface NumberTemplate extends TemplateField<TemplateFieldType.NUMBER> {
+  options?: {
+    default?: string;
+    min?: number;
+    max?: number;
+    placeholder?: string;
+  };
+}
+export interface StringTemplate extends TemplateField<TemplateFieldType.STRING> {
+  options?: {
+    default?: string;
+    placeholder?: string;
+  };
+}
+export interface EnumTemplate extends TemplateField<TemplateFieldType.ENUM> {
+  options: {
+    default?: string;
+    values: EnumChoices;
+  };
+}
+export interface NodeTemplate extends TemplateField<TemplateFieldType.NODE> {
+  options?: {
+    categories?: string[];
+  };
+}
+export interface NodesetTemplate extends TemplateField<TemplateFieldType.NODE_SET> {
+  options?: {
+    categories?: string[];
+  };
+}
+export interface DateTemplate extends TemplateField<TemplateFieldType.DATE> {
+  options: {
+    default?: string;
+    min?: string;
+    max?: string;
+    format: DateFormat;
+  };
+}
+export interface DatetimeTemplate extends TemplateField<TemplateFieldType.DATE_TIME> {
+  options: {
+    default?: string;
+    min?: string;
+    max?: string;
+    timezone?: string;
+    format: DatetimeFormat;
+  };
+}
+export interface BooleanTemplate extends TemplateField<TemplateFieldType.BOOLEAN> {
+  options?: {
+    default?: boolean;
+  };
+}
+
+
+export enum GraphQuerySharingMode {
+  PRIVATE = 'private',
+  SOURCE = 'source',
+  GROUPS = 'groups'
+}
+
+export enum GraphQueryDialect {
+  CYPHER = 'cypher',
+  GREMLIN = 'gremlin'
+}
+
+export enum GraphQueryType {
+  STATIC = 'static',
+  TEMPLATE = 'template'
+}
+
+export enum GraphInputType {
+  NONE = "none",
+  _1_NODE = "1-node",
+  _2_NODES = "2-nodes",
+  NODESET = "nodeset"
+}
+
+export enum RIGHT_VALUES {
+  OWNER = 'owner',
+  READ = 'read'
+}
+
+export interface IGetGraphQueryParams extends IDataSourceParams {
+  id: number;
+}
+
+export interface IGraphQueryResponse {
+  id: number;
+  sourceKey: string;
+  name: string;
+  content: string;
+  dialect: GraphQueryDialect;
+  description: string;
+  sharing: GraphQuerySharingMode;
+  sharedWithGroups?: number[];
+  write: boolean;
+  graphInput?: GraphInputType;
+  templateFields?: TemplateFields;
+  type: GraphQueryType;
+  right: RIGHT_VALUES;
+  builtin: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IGetGraphQueriesParams extends  IDataSourceParams {
+  type: GraphQueryType
+}
+
+export interface IRunGraphQueryParams extends IDataSourceParams {
+  dialect?: string;
+  limit?: number;
+  timeout?: number;
+  edgesTo?: Array<string | number>;
+  withAccess?: boolean;
+  withDegree?: boolean;
+  withDigest?: boolean;
+  templateData?: GenericObject<unknown>;
+}
+
+export interface IRunGraphQueryByContentParams extends IRunGraphQueryParams {
+  query: string;
+}
+
+export interface IRunGraphQueryByIdParams extends IRunGraphQueryParams {
+  id: number;
+}
+
+export interface IRunGraphQueryResponse {
+  nodes: LkNode[];
+  edges: LkEdge[];
+  truncatedByLimit: boolean;
+  truncatedByAccess: boolean;
+}
+
+export interface ICheckGraphQueryParams extends IDataSourceParams {
+  query: string;
+  dialect?: GraphQueryDialect;
+}
+
+export interface ICheckGraphQueryResponse {
+  write: boolean;
+  type: GraphQueryType;
+  graphInput?: GraphInputType;
+  templateFields?: TemplateField[];
+}
+
+export enum COLUMN_TYPE_VALUES {
+  STRING = 'string',
+  NUMBER = 'number'
+}
+
+export interface IAlertPreviewParams {
+  query: string;
+  dialect?: GraphQueryDialect;
+  columns?: Array<{columnName: string; columnTitle?: string; type: COLUMN_TYPE_VALUES}>;
+  limit?: number;
+  timeout?: number;
+}
+export interface IAlertPreviewResponse {
+  results: Array<{
+    nodes: LkNode[];
+    edges: LkEdge[];
+    columns: Array<string | number>;
+  }>;
+}
+
+export interface ICreateGraphQueryParams extends IDataSourceParams {
+  name: string;
+  content: string;
+  dialect?: GraphQueryDialect;
+  description: string;
+  sharing: GraphQuerySharingMode;
+  sharedWithGroups?: number[];
+
+}
+
+export interface IUpdateGraphQueryParams extends Partial<ICreateGraphQueryParams> {
+  id: number;
+}
+
+export interface IDeleteGraphQueryParams extends IDataSourceParams {
+  id: number;
+}
