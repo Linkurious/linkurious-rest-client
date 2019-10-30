93b6f2355b60ccf34eb533d9af7175fbcbdc1096
Cheick Diarra <cheickdiarra74@gmail.com>
2019-10-30 16:31:37 +0100

    migrate visualization module

---
+++ src/_src/api/Visualization/index.ts
@@ -0,0 +1,323 @@
+/**
+ * LINKURIOUS CONFIDENTIAL
+ * Copyright Linkurious SAS 2012 - 2019
+ *
+ * - Created on 2019-10-30.
+ */
+import { Request } from '../../http/request';
+import { LkErrorKey } from '../../http/response';
+import { IDataSourceParams } from '../commonTypes';
+import { Utils } from '../../utils';
+
+import {
+  ICreateVisualizationFolderParams,
+  ICreateVisualizationFolderResponse,
+  ICreateVisualizationParams,
+  ICreateWidgetParams,
+  IDeleteFolderParams,
+  IDeleteVisualizationParams,
+  IDeleteWidgetParams,
+  IDuplicateVisualizationParams,
+  IDuplicateVisualizationResponse,
+  IGetVisualizationByIdParams,
+  IGetVisualizationSandboxParams,
+  IGetVisualizationSharesPrams,
+  IGetWidgetParams,
+  IGetWidgetResponse,
+  IMergeVisualizationsParams,
+  ISharedVisualization,
+  IShareVisualizationParams,
+  IUnshareVisualizationParams,
+  IUpdateSandboxParams,
+  IUpdateVisualizationFolderParams,
+  IUpdateVisualizationParams,
+  IUpdateWidgetParams,
+  IVisualizationResponse,
+  IVisualizationShare, IVisualizationShares, IVisualizationTree
+} from './types';
+
+const {INVALID_PARAMETER, UNAUTHORIZED, FORBIDDEN, NOT_FOUND} = LkErrorKey;
+
+export class VisualizationApi extends Request {
+  public mergeVisualizations(params: IMergeVisualizationsParams) {
+    return this
+      .handle(UNAUTHORIZED, FORBIDDEN)
+      .request({
+        url: '/admin/users/mergeVisualizations',
+        method: 'POST',
+        params: params
+      }
+    );
+  }
+
+  public async updateVisualizationFolder(params: IUpdateVisualizationFolderParams) {
+    return this
+      .handle(UNAUTHORIZED, FORBIDDEN, NOT_FOUND, INVALID_PARAMETER)
+      .request({
+        url: '/{sourceKey}/visualizations/folder/{id}',
+        method: 'PATCH',
+        params: params
+    });
+  }
+
+  /**
+   * Get shared visualizations.
+   */
+  public getShared(params: IDataSourceParams) {
+    return this
+      .handle(INVALID_PARAMETER)
+      .request<ISharedVisualization[]>({
+        url: '/{sourceKey}/visualizations/shared',
+        method: 'GET',
+        params: params
+      }
+    );
+  }
+
+  /**
+   * Get the number of visualizations for the current user in this data-source.
+   *
+   * @breakingChange update this api on the server to return number
+   */
+  public count(params: IDataSourceParams) {
+    return this
+      .handle(INVALID_PARAMETER)
+      .request<number>({
+        url: '/{sourceKey}/visualizations/count',
+        method: 'GET',
+        params: params
+    });
+  }
+
+  /**
+   * Create a widget for a visualization.
+   */
+  public createWidget(params: ICreateWidgetParams) {
+    return this
+      .handle(UNAUTHORIZED, FORBIDDEN, INVALID_PARAMETER)
+      .request<string>({
+        url: '/widget',
+        method: 'POST',
+        params: params
+      });
+  }
+
+  /**
+   * Update a widget for a visualization.
+   */
+  public updateWidget(params: IUpdateWidgetParams) {
+    return this
+      .handle(INVALID_PARAMETER, UNAUTHORIZED, FORBIDDEN, NOT_FOUND)
+      .request<string>({
+        url: '/widget',
+        method: 'PUT',
+        params: params
+      }
+    );
+  }
+
+  /**
+   * Create a folder for visualizations.
+   *
+   * @breakingChange update the server api to return the folder directly
+   */
+  public createFolder(params: ICreateVisualizationFolderParams) {
+    return this
+      .handle(INVALID_PARAMETER, UNAUTHORIZED)
+      .request<ICreateVisualizationFolderResponse>({
+        url: '/{sourceKey}/visualizations/folder',
+        method: 'POST',
+        params: params
+    });
+  }
+
+  /**
+   * Create a new visualization.
+   */
+  public create(params: ICreateVisualizationParams) {
+    return this
+      .handle(INVALID_PARAMETER, UNAUTHORIZED)
+      .request<IVisualizationResponse>({
+        url: '/{sourceKey}/visualizations',
+        method: 'POST',
+        params: params
+    });
+  }
+
+  /**
+   * Delete a widget for a visualization.
+   */
+  public deleteWidget(params: IDeleteWidgetParams) {
+    return this
+      .handle(INVALID_PARAMETER, UNAUTHORIZED, FORBIDDEN)
+      .request({
+        url: '/widget/{id}',
+        method: 'DELETE',
+        params: params
+    });
+  }
+
+  /**
+   * Remove the specified folder and its children (visualizations and sub-folders).
+   */
+  public deleteFolder(params: IDeleteFolderParams) {
+    return this
+      .handle(INVALID_PARAMETER, UNAUTHORIZED, FORBIDDEN)
+      .request({
+        url: '/{sourceKey}/visualizations/folder/{id}',
+        method: 'DELETE',
+        params: params
+    });
+  }
+
+  /**
+   * Duplicates a visualization.
+   */
+  public duplicate(params: IDuplicateVisualizationParams) {
+    return this
+      .handle(INVALID_PARAMETER, UNAUTHORIZED, FORBIDDEN)
+      .request<IDuplicateVisualizationResponse>({
+        url: '/{sourceKey}/visualizations/{id}/duplicate',
+        method: 'POST',
+        params: params
+      }
+    );
+  }
+
+  /**
+   * Get a visualization widget's data by key.
+   */
+  public getWidget(params: IGetWidgetParams) {
+    return this
+      .handle(INVALID_PARAMETER, UNAUTHORIZED, FORBIDDEN)
+      .request<IGetWidgetResponse>({
+        url: '/widget/{id}',
+        method: 'GET',
+        params: params
+    });
+  }
+
+  /**
+   * Return the visualization sandbox of the current user for a given data-source.
+   */
+  public getSandbox(params: IGetVisualizationSandboxParams) {
+    return this
+      .handle(INVALID_PARAMETER, UNAUTHORIZED)
+      .request<IVisualizationResponse>({
+        url: '/{sourceKey}/sandbox',
+        method: 'GET',
+        params: params
+    });
+  }
+
+  /**
+   * Return one visualizations selected by ID.
+   */
+  public getOne(params: IGetVisualizationByIdParams) {
+    return this
+      .handle(INVALID_PARAMETER, UNAUTHORIZED, FORBIDDEN)
+      .request<IVisualizationResponse>({
+        url: '/{sourceKey}/visualizations/{id}',
+        method: 'GET',
+        params: params
+    });
+  }
+
+  /**
+   * Return visualizations ordered with folders hierarchy.
+   *
+   * @breakingChange update the server api to return the tree directly
+   */
+  public getTree(params: IDataSourceParams) {
+    return this
+      .handle(INVALID_PARAMETER, UNAUTHORIZED, FORBIDDEN)
+      .request<IVisualizationTree[]>({
+        url: '/{sourceKey}/visualizations/tree',
+        method: 'GET',
+        params: params
+    });
+  }
+
+  /**
+   * Remove visualization selected by id.
+   */
+  public deleteOne(params: IDeleteVisualizationParams) {
+    return this
+      .handle(INVALID_PARAMETER, UNAUTHORIZED, FORBIDDEN, NOT_FOUND)
+      .request({
+        url: '/{sourceKey}/visualizations/{id}',
+        method: 'DELETE',
+        params: params
+    });
+  }
+
+  /**
+   * Get all share rights on a visualization.
+   */
+  public getShares(params: IGetVisualizationSharesPrams) {
+    return this
+      .handle(INVALID_PARAMETER, UNAUTHORIZED, FORBIDDEN)
+      .request<IVisualizationShares>({
+        url: '/{sourceKey}/visualizations/{id}/shares',
+        method: 'GET',
+        params: params
+      }
+    );
+  }
+
+  /**
+   * Set the share right of a user on a visualization.
+   */
+  public share(params: IShareVisualizationParams) {
+    return this
+      .handle(INVALID_PARAMETER, UNAUTHORIZED, FORBIDDEN)
+      .request<IVisualizationShare>({
+        url: `/{sourceKey}/visualizations/:vizId/share/:userId`,
+        method: 'PUT',
+        params: params
+      }
+    );
+  }
+
+
+  /**
+   * Remove a share right of a user on a visualization.
+   */
+  public unshare(params: IUnshareVisualizationParams) {
+    return this
+      .handle(INVALID_PARAMETER, UNAUTHORIZED, FORBIDDEN)
+      .request({
+        url: `/{sourceKey}/visualizations/{id}/share/:userId`,
+        method: 'DELETE',
+        params: params
+    });
+  }
+
+  /**
+   * Update the sandbox of the current user for a given data-source.
+   */
+  public updateSandbox(params: IUpdateSandboxParams){
+    return this
+      .handle(INVALID_PARAMETER, UNAUTHORIZED, FORBIDDEN)
+      .request({
+        url: '/{sourceKey}/sandbox',
+        method: 'PATCH',
+        params: params
+    });
+  }
+
+  /**
+   * Update visualization selected by id.
+   */
+  public update(params: IUpdateVisualizationParams) {
+    return this
+      .handle(INVALID_PARAMETER, UNAUTHORIZED, FORBIDDEN)
+      .request({
+        url: '/{sourceKey}/visualizations/{id}',
+        method: 'PATCH',
+        params: params,
+        query: Utils.fixSnakeCase({forceLock: params.forceLock})
+      }
+    );
+  }
+}
---
+++ src/_src/api/Visualization/types.ts
@@ -0,0 +1,278 @@
+import { VizEdge, VizNode } from '../..';
+
+/**
+ * LINKURIOUS CONFIDENTIAL
+ * Copyright Linkurious SAS 2012 - 2019
+ *
+ * - Created on 2019-10-30.
+ */
+import {IDataSourceParams} from '../commonTypes';
+import { ISimpleUser } from '../User/types';
+
+
+export interface IMergeVisualizationsParams {
+  from: number;
+  to: number;
+}
+
+export interface IUpdateVisualizationFolderParams extends IDataSourceParams {
+  id: number;
+  title?: string;
+  parent?: number;
+}
+
+export interface IVisualizationDesign {
+  styles?: any;
+  palette?: any;
+}
+
+export interface IVisualizationGeo {
+  latitudeProperty?: string;
+  longitudeProperty?: string;
+  layers?: string[];
+}
+
+
+export interface IAlternativeIdConfig {
+  node: string;
+  edge: string;
+}
+
+export interface IVisualizationLayout {
+  algorithm?: string;
+  mode?: string;
+}
+
+export enum VisuslizationMode {
+  NODE_LINK = 'nodelink',
+  GEO = 'geo'
+}
+
+export interface IWidgetUI {
+  search?: boolean;
+  share?: boolean;
+  layout?: boolean;
+  fullscreen?: boolean;
+  zoom?: boolean;
+  legend?: boolean;
+  geo?: boolean;
+}
+
+export interface IVisualizationResponse {
+  id: number;
+  design: IVisualizationDesign;
+  nodeFields: IItemFields;
+  edgeFields: IItemFields;
+  sourceKey: string;
+  title: string;
+  folder: number;
+  nodes: VizNode[];
+  edges: VizEdge[];
+  alternativeIds: IAlternativeIdConfig;
+  layout: IVisualizationLayout;
+  geo: IVisualizationGeo;
+  mode: VisuslizationMode;
+  right: VisualizationRights;
+  filters: any[];
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface IWidgetContent extends IVisualizationDesign {
+  title?: string;
+  description?: string;
+  url?: string;
+  mode?: string;
+  mapLayers?: any[];
+  ui?: IWidgetUI;
+}
+
+
+export interface ICreateWidgetParams {
+  visualizationId: number;
+  content?: IWidgetContent;
+}
+
+export interface IUpdateWidgetParams extends  ICreateWidgetParams {}
+
+export interface ICreateVisualizationFolderParams {
+  title: string;
+  parent: number;
+}
+
+export interface ICreateVisualizationFolderResponse {
+  id: number;
+  title: string;
+  parent: number;
+  sourceKey: string;
+}
+
+export interface ISharedVisualization extends IDataSourceParams {
+  title: string;
+  visualizationId: number;
+  ownerId: number;
+}
+
+export enum VisualizationRights {
+  READ = 'read',
+  WRITE = 'write',
+  OWNER = 'owner'
+}
+export interface IBaseShare {
+  userId: number;
+  right: VisualizationRights;
+  visualizationId: number;
+}
+
+export interface ISharer extends IBaseShare {
+  username: string;
+  email: string;
+}
+
+export interface SharedWith {
+  owner: ISimpleUser;
+  shares: ISharer[];
+  userId: number;
+  right: VisualizationRights;
+  visualizationId: number;
+}
+
+export interface IVisualizationShares {
+  owner: ISimpleUser;
+  shares: SharedWith[];
+}
+
+export interface ICreateVisualizationParams extends IDataSourceParams {
+  title: string;
+  folder?: number;
+  nodes: VizNode[];
+  edges: VizEdge[];
+  alternativeIds?: IAlternativeIdConfig;
+  layout?: IVisualizationLayout;
+  mode?: string;
+  geo?: IVisualizationGeo;
+  design?: IVisualizationDesign;
+  filters?: any[];
+  nodeFields: IItemFields;
+  edgeFields: IItemFields;
+}
+
+export interface IDeleteWidgetParams {
+  id: string;
+}
+
+export interface IDeleteFolderParams {
+  id: number;
+}
+
+export interface IDuplicateVisualizationParams {
+  id: number;
+  title?: string;
+  folder?: number;
+}
+
+export interface IDuplicateVisualizationResponse {
+  visualizationId: number;
+}
+
+export interface IGetWidgetParams {
+ id: string;
+}
+
+export interface IGetWidgetResponse {
+  title: string;
+  key: string;
+  userId: number;
+  visualizationId: number;
+  content: IWidgetContent;
+}
+
+export enum PopulateType {
+  EXPAND_NODE_ID = 'expandNodeId',
+  NODE_ID = 'nodeId',
+  EDGE_ID = 'edgeId',
+  SEARCH_NODE = 'searchNodes',
+  SEARCH_EDGE = 'searchEdges',
+  PATTERN = 'pattern'
+}
+
+export interface IGetVisualizationSandboxParams {
+  populate?: PopulateType;
+  itemId?: number;
+  searchQuery?: string;
+  searchFuzziness?: number;
+  doLayout?: boolean;
+  patternDialect?: string;
+  patternQuery?: boolean;
+  withDigest?: boolean;
+  withDegree?: boolean;
+  matchId?: boolean;
+}
+
+export interface IGetVisualizationByIdParams {
+  id: number;
+  populated?: boolean;
+  withDigest?: boolean;
+  withDegree?: boolean;
+}
+
+export interface IVisualizationTree {
+  type: 'visu' | 'folder';
+  id: number;
+  title: string;
+  children?: IVisualizationTree[];
+  shareCount?: number;
+  widgetKey?: string;
+}
+
+export interface IDeleteVisualizationParams {
+  id: number;
+}
+
+export interface IGetVisualizationSharesPrams extends IDataSourceParams {
+  id: number;
+}
+
+export interface IShareVisualizationParams extends IDataSourceParams {
+  userId: number;
+  right?: string;
+  vizId: number;
+}
+
+export interface IVisualizationShare {
+  userId: number;
+  right: VisualizationRights;
+  visualizationId: number;
+  updatedAt: string;
+  createdAt: string;
+}
+
+export interface IUnshareVisualizationParams extends IDataSourceParams {
+  id: number;
+  userId: number;
+}
+
+export interface IItemFields {
+  captions: {[key: string]: {displayName: boolean; properties: string[]; active: boolean}};
+  types: {[key: string]: string};
+}
+
+export interface IUpdateSandboxParams extends IDataSourceParams {
+  design?: IVisualizationDesign;
+  nodeFields?: IItemFields;
+  edgeFields?: IItemFields;
+}
+
+export interface IUpdateVisualizationParams extends Partial<ICreateVisualizationParams> {
+  id: number;
+  forceLock?: boolean;
+}
+
+export interface IVisualizationTree {
+  type: 'visu' | 'folder';
+  id: number;
+  title: string;
+  children?: IVisualizationTree[];
+  shareCount?: number;
+  widgetKey?: string;
+}
