5844d1cfbc52089aa5b7993ed251cb933e343159
Francesco Infante <francesco@linkurio.us>
2019-10-31 13:28:50 +0100

    Added remaining modules to index file

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import {AlertsAPI} from './api/Alerts';
 import {ApplicationsAPI} from './api/Applications';
 import {AuthAPI} from './api/Auth';
 import {CustomActionAPI} from './api/CustomAction';
-import {DataSourceAPI} from './api/DataSource';
+import {DataSourceAPI, IUserDataSource} from './api/DataSource';
 import {GraphAPI} from './api/Graph';
 import {GraphEdgeAPI} from './api/GraphEdge';
 import {GraphNodeAPI} from './api/GraphNode';
@@ -26,54 +26,58 @@ import {VisualizationAPI} from './api/Visualization';
 export class RestClient extends ErrorListener {
   private readonly moduleProps: ModuleProps;
 
-  public alerts: AlertsAPI;
-  public applications: ApplicationsAPI;
-  public auth: AuthAPI;
-  public customAction: CustomActionAPI;
-  public dataSource: DataSourceAPI;
-  public graph: GraphAPI;
-  public graphEdge: GraphEdgeAPI;
-  public graphNode: GraphNodeAPI;
-  public graphQuery: GraphQueryAPI;
-  public graphSchema: GraphSchemaAPI;
-  public linkurious: LinkuriousAPI;
-  public user: UserAPI;
-  public visualization: VisualizationAPI;
+  public readonly alerts: AlertsAPI;
+  public readonly applications: ApplicationsAPI;
+  public readonly auth: AuthAPI;
+  public readonly customAction: CustomActionAPI;
+  public readonly dataSource: DataSourceAPI;
+  public readonly graph: GraphAPI;
+  public readonly graphEdge: GraphEdgeAPI;
+  public readonly graphNode: GraphNodeAPI;
+  public readonly graphQuery: GraphQueryAPI;
+  public readonly graphSchema: GraphSchemaAPI;
+  public readonly linkurious: LinkuriousAPI;
+  public readonly user: UserAPI;
+  public readonly visualization: VisualizationAPI;
 
-  constructor(props?: {baseUrl?: string; agent?: request.SuperAgentStatic}) {
+  constructor(options?: {baseUrl?: string; agent?: request.SuperAgentStatic}) {
     super();
 
     this.moduleProps = {
-      baseUrl: props
-        ? props.baseUrl && props.baseUrl.endsWith('/')
-          ? props.baseUrl + 'api'
-          : props.baseUrl + '/api'
+      baseUrl: options
+        ? options.baseUrl && options.baseUrl.endsWith('/')
+          ? options.baseUrl + 'api'
+          : options.baseUrl + '/api'
         : '/api',
-      agent: (props && props.agent) || request.agent(),
+      agent: (options && options.agent) || request.agent(),
       clientState: {},
       dispatchError: (key: LkErrorKey, payload: unknown): void => this.dispatchError(key, payload)
     };
-    this.linkurious = new LinkuriousAPI(this.moduleProps);
-    this.graphSchema = new GraphSchemaAPI(this.moduleProps);
-    this.graphNode = new GraphNodeAPI(this.moduleProps);
-    this.graphEdge = new GraphEdgeAPI(this.moduleProps);
+
     this.alerts = new AlertsAPI(this.moduleProps);
-    this.customAction = new CustomActionAPI(this.moduleProps);
+    this.applications = new ApplicationsAPI(this.moduleProps);
     this.auth = new AuthAPI(this.moduleProps);
+    this.customAction = new CustomActionAPI(this.moduleProps);
     this.dataSource = new DataSourceAPI(this.moduleProps);
+    this.graph = new GraphAPI(this.moduleProps);
+    this.graphEdge = new GraphEdgeAPI(this.moduleProps);
+    this.graphNode = new GraphNodeAPI(this.moduleProps);
+    this.graphQuery = new GraphQueryAPI(this.moduleProps);
+    this.graphSchema = new GraphSchemaAPI(this.moduleProps);
+    this.linkurious = new LinkuriousAPI(this.moduleProps);
+    this.user = new UserAPI(this.moduleProps);
+    this.visualization = new VisualizationAPI(this.moduleProps);
   }
 
-  get clientState(): IClientState {
+  public get clientState(): IClientState {
     return this.moduleProps.clientState;
   }
 
+  // TODO refresh data-source status on API (deleteFullDataSource, deleteDataSourceConfig)
   /**
-   * Process to login and set the default source state and return the REST client state.
-   *
-   * @param {Object} data
-   * @returns {Promise<IClientState>}
+   * Login a user and populate the client state with the list of the data-sources.
    */
-  async init(data: {usernameOrEmail: string; password: string}): Promise<void> {
+  public async init(data: {usernameOrEmail: string; password: string}): Promise<void> {
     await this.auth.login(data);
     await this.dataSource.getDataSourcesStatus({
       withCaptions: true,
@@ -81,43 +85,32 @@ export class RestClient extends ErrorListener {
     });
   }
 
-  /*
-     TODO: Update sources in I add it to
-      [ ] `admin.deleteFullDataSource()`,
-      [ ] `admin.deleteDataSourceConfig()` and
-      [X] `getUserDataSources()`
-   */
-
-  /*
-    throws Error
-   */
-  static getCurrentSource(
+  public static getCurrentSource(
     dataSources: IUserDataSource[],
-    by?: {userId: number} | {sourceKey: string} | {sourceIndex: number}
+    by?: {userId: number} | {sourceKey: string} | {configIndex: number}
   ): IUserDataSource {
     if (!dataSources.length) {
-      throw new Error('RestClient::getCurrentSource - Datasources cannot be empty');
+      throw new Error('RestClient::getCurrentSource - dataSources cannot be empty.');
     }
 
     if (by) {
       let source: IUserDataSource | undefined;
       if ('userId' in by) {
-        // Return last seen dataSource by user in localstorage if it's connected
+        // Return the last seen data-source by the current user if the data-source is connected
         try {
-          const sourceKey: string | null = localStorage.getItem(
-            'lk-lastSeenSourceKey-' + by.userId
-          );
+          const sourceKey = localStorage.getItem('lk-lastSeenSourceKey-' + by.userId);
           source = dataSources.find(s => s.connected && s.key === sourceKey);
         } catch (_) {
           source = undefined;
         }
       } else if ('sourceKey' in by) {
-        // Return dataSource whose key matches sourceKey
+        // Return the data-source whose sourceKey matches sourceKey in input
         source = dataSources.find(s => s.connected && s.key === by.sourceKey);
       } else {
-        // Return dataSource whose index matches configIndex
-        source = dataSources.find(s => s.connected && s.configIndex === by.sourceIndex);
+        // Return the data-source whose configIndex matches configIndex in input
+        source = dataSources.find(s => s.connected && s.configIndex === by.configIndex);
       }
+
       if (source) {
         return source;
       }
@@ -129,6 +122,7 @@ export class RestClient extends ErrorListener {
         return firstConnected;
       }
     }
+
     // Return the first data-source
     return dataSources[0];
   }
