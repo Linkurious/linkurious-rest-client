d03aab36f29a88851a0c7204fc5dfee8e8d14daa
Jose Renato Ramos Gonzalez <renato@linkurio.us>
2019-11-13 15:20:15 +0100

    added sourceKey to AlertFolder in server

--- src/api/Alerts/types.ts
+++ src/api/Alerts/types.ts
@@ -62,7 +62,6 @@ export interface ICreateAlertFolderParams extends IDataSourceParams {
   title: string;
 }
 
-// TODO add sourceKey to server
 export interface AlertFolder extends PersistedItem {
   title: string;
   parent: number;
