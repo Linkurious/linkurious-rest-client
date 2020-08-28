481b63fb7991e1f483fd76c48e5b12a68cfd82fa
Yiğit Akbulut <yigit@linkurio.us>
2020-08-28 14:15:47 +0300

    LKE-418-Standart-connection-timeout
    Added parameter to rest-client for maxConnectionTimeout

--- src/api/Config/types.ts
+++ src/api/Config/types.ts
@@ -246,6 +246,7 @@ export interface IAdvancedConfig {
   searchAddAllThreshold: number;
   minSearchQueryLength: number;
   rawQueryTimeout: number;
+  maxConnectionTimeout?: number;
   sampledItemsPerType: number;
   sampledVisualizationItems: number;
   defaultTimeZone: string;
