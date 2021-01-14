49887b12d682a48aac67f6ba89e719bbd6caf77d
mzorkany <mahmoud@linkuriou.us>
2021-01-14 17:45:35 +0100

    [do_nexus_upload]

--- src/api/DataSource/types.ts
+++ src/api/DataSource/types.ts
@@ -115,6 +115,7 @@ export interface IDeleteSourceConfigParams {
 }
 
 /**
+ * 
  * A DataSourceAdminInfo can represent:
  * - a disconnected data-source configuration
  * - a disconnected data-source state not configured anymore
