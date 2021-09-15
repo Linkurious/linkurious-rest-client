644c949bebb9ae58d48eda56846de99b29f88689
Eric Mallard <eric.delavarende@linkurio.us>
2021-09-15 11:23:42 +0200

    fix: alert description can be null (on update)

--- src/api/Alert/types.ts
+++ src/api/Alert/types.ts
@@ -52,7 +52,7 @@ export interface ICreateAlertParams extends Omit<IBaseAlert, 'folder'> {
 
 export interface IBaseAlert extends IDataSourceParams, SharingOptions {
   title: string;
-  description?: string;
+  description?: string | null;
   query: string;
   dialect: GraphQueryDialect;
   folder: number;
