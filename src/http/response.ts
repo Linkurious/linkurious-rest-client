25e007cebf4e24bfb4a0829931fe42e3ab083dfa
Guillaume Masclet <guillaume.masclet@linkurio.us>
2022-03-31 14:42:44 +0200

    LKE-5283: Remove LkErrorKey.STORE_ID_CHANGED

--- src/http/response.ts
+++ src/http/response.ts
@@ -54,8 +54,7 @@ export enum LkErrorKey {
   PLUGIN_NOT_READY = 'plugin_not_ready',
   PLUGIN_SERVICE_NOT_READY = 'plugin_service_not_ready',
   INVALID_CONFIGURATION = 'invalid_configuration',
-  SEND_MAIL_FAILED = 'send_mail_failed',
-  STORE_ID_CHANGED = 'store_id_changed'
+  SEND_MAIL_FAILED = 'send_mail_failed'
 }
 
 /**
@@ -230,8 +229,6 @@ export interface SearchDisabledError extends LkError<LkErrorKey.SEARCH_DISABLED>
 
 export interface SendMailFailed extends LkError<LkErrorKey.SEND_MAIL_FAILED> {}
 
-export interface StoreIdChangedError extends LkError<LkErrorKey.STORE_ID_CHANGED> {}
-
 // Mapping from LkErrorKey to LkError, it's used by `ErrorResponses`
 export type LkErrorKeyToInterface = {
   [LkErrorKey.CONNECTION_REFUSED]: ConnectionRefusedError;
@@ -272,5 +269,4 @@ export type LkErrorKeyToInterface = {
   [LkErrorKey.SEARCH_DISABLED]: SearchDisabledError;
   [LkErrorKey.REDUNDANT_ACTION]: RedundantActionError;
   [LkErrorKey.SEND_MAIL_FAILED]: SendMailFailed;
-  [LkErrorKey.STORE_ID_CHANGED]: StoreIdChangedError;
 };
