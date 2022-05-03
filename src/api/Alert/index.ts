0fee14efb71bec64f181a44b453948bc1b4f51f3
Eric Mallard <eric.delavarende@linkurio.us>
2022-05-03 10:33:48 +0200

    make param optional to gell all users

--- src/api/Alert/index.ts
+++ src/api/Alert/index.ts
@@ -347,7 +347,7 @@ export class AlertAPI extends Request {
   /**
    * Find all the users that can process the alerts which are accessible to the current user
    */
-  public getAllAlertsUsers(this: Request<IBasicUser[]>, params: IDataSourceParams) {
+  public getAllAlertsUsers(this: Request<IBasicUser[]>, params?: IDataSourceParams) {
     return this.request({
       errors: [UNAUTHORIZED, DATA_SOURCE_UNAVAILABLE, FORBIDDEN, NOT_FOUND],
       url: '/:sourceKey/alerts/users',
