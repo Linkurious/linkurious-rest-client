6e30abe7d3abce3c00d60920f949b1d87cef7bd3
mzorkany <mahmoud@linkurio.us>
2022-11-15 16:19:57 +0100

    Update types.ts

--- src/api/Alert/types.ts
+++ src/api/Alert/types.ts
@@ -68,7 +68,7 @@ export interface ICreateAlertParams extends Omit<IBaseAlert, 'folder' | 'queries
 
 export interface ICreateAlertQueryParams extends Omit<IAlertQuery, 'id'> {}
 
-export interface IUpdateAlertQueryParams extends Omit<Partial<IAlertQuery>, 'id'> {
+export interface IUpdateAlertQueryParams extends Omit<IAlertQuery, 'id'> {
   id?: number;
 }
 
