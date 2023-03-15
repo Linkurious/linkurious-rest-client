ca74e411913854d4b4b2d588c257f1ef38e35330
mzorkany <mahmoud@linkurio.us>
2023-03-15 11:17:00 +0100

    LKE-7492 rename type AlertRunPRoblem to AlertError

--- src/api/Alert/types.ts
+++ src/api/Alert/types.ts
@@ -90,7 +90,7 @@ export interface Alert extends IBaseAlert, PersistedItem {
   sourceKey: string;
   lastRun?: string; // defined if it has run at least once
   // defined if last run had a problem
-  lastRunProblem?: AlertRunProblem[];
+  lastRunProblem?: AlertError[];
   nextRun?: string; // defined if enabled=true
   openAndUnAssignedCasesCount: number;
   status: 'running' | 'idle';
@@ -105,12 +105,12 @@ export interface IAlertQuery {
   dialect: GraphQueryDialect;
 }
 
-type AlertRunProblem = {
+type AlertError = {
   queryId?: number;
   source: 'caseAttributeQuery' | 'alertQuery';
   error: LkError;
   partial: boolean;
-} | null;
+};
 
 export interface IRunAlertParams extends IDataSourceParams {
   id: number;
@@ -118,7 +118,7 @@ export interface IRunAlertParams extends IDataSourceParams {
 }
 export interface RunAlertResponse {
   alreadyRunning: boolean;
-  runProblems?: AlertRunProblem[];
+  runProblems?: AlertError[];
 }
 
 export interface IUpdateAlertParams extends Omit<Partial<ICreateAlertParams>, 'queries'> {
