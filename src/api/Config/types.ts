b9eb463d436ef7bcb3252219550f5f16bbaeed23
amine@linkurio.us <amine@linkurio.us>
2023-03-24 11:07:46 +0100

    keeping DASHBOARD to support client migrating from old version

--- src/api/Config/types.ts
+++ src/api/Config/types.ts
@@ -245,8 +245,10 @@ export interface ILeafletConfig {
 }
 
 export enum DefaultPage {
-  SPACES = 'spaces',
-  WORKSPACE = 'workspace'
+  // keeping DASHBOARD to keep supporting customer migrating from old LKE version
+  DASHBOARD = 'dashboard',
+  WORKSPACE = 'workspace',
+  SPACES = 'spaces'
 }
 
 export interface IAccessConfig {
