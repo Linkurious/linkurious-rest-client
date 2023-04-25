592f1427d0a450ed5e27d2f2da6e24ace5f12983
mzorkany <mahmoud@linkurio.us>
2023-04-25 17:55:58 +0200

    LKE-8666 run lint

--- src/api/GraphQuery/types.ts
+++ src/api/GraphQuery/types.ts
@@ -233,7 +233,7 @@ export interface IDeleteQueryParams extends IDataSourceParams {
 export interface ICheckQueryParams extends IDataSourceParams {
   query: string;
   dialect?: GraphQueryDialect;
-  isAlertQuery?:boolean;
+  isAlertQuery?: boolean;
   isCaseAttributesQuery?: boolean;
   expectedOutputFields?: string[];
 }
