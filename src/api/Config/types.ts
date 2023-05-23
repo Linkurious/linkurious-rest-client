22ccda40ce44de3f8c574e2ef92094cb8f0bb35d
Eric Mallard <eric.delavarende@linkurio.us>
2023-05-23 14:59:05 +0200

    add localAuth flag to config

--- src/api/Config/types.ts
+++ src/api/Config/types.ts
@@ -252,6 +252,7 @@ export interface ILeafletConfig {
 export interface IAccessConfig {
   floatingLicenses?: number;
   authRequired?: boolean;
+  localAuth: boolean;
   guestMode?: boolean;
   dataEdition?: boolean;
   widget?: boolean;
