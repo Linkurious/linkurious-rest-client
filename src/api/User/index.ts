c34948a39ac828d61943fee2db055be0be4fa04e
aminerhanemi <amine@linkurio.us>
2023-06-30 14:32:07 +0200

    add INVALID_PARAMETER to list of errors

--- src/api/User/index.ts
+++ src/api/User/index.ts
@@ -245,7 +245,7 @@ export class UserAPI extends Request {
    */
   public getGroupUsers(this: Request<SearchUsersSimpleResponse>, params: GetGroupUsersParams) {
     return this.request({
-      errors: [UNAUTHORIZED, FORBIDDEN, DATA_SOURCE_UNAVAILABLE],
+      errors: [UNAUTHORIZED, FORBIDDEN, DATA_SOURCE_UNAVAILABLE, INVALID_PARAMETER],
       url: '/:sourceKey/groups/:groupId/users',
       method: 'GET',
       params: params
