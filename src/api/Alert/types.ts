c0d75f6f7c35b4a01181f4b7d1895775c0215de2
Hussein Bassam <husseinbassam95@gmail.com>
2023-07-31 14:41:46 +0200

    add options to caseColumn

--- src/api/Alert/types.ts
+++ src/api/Alert/types.ts
@@ -328,6 +328,7 @@ export interface ICaseColumn {
   type: AlertColumnType;
   columnValue: string | number | null;
   columnTitle: string;
+  options: ICurrencyOptions | null;
 }
 
 export interface AlertQueryData {
