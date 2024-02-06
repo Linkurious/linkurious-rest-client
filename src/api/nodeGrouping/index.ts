a49bdc5e6ca6397cae00c640670a881d770e3852
mzorkany <mahmoud@linkurio.us>
2024-02-06 18:12:55 +0100

    Update index.ts

--- src/api/nodeGrouping/index.ts
+++ src/api/nodeGrouping/index.ts
@@ -7,12 +7,15 @@
 import {LkErrorKey} from '../../http/response';
 import {Request} from '../../http/request';
 
-import {CreateNodeGroupingRuleParams} from './types';
+import {CreateNodeGroupingRuleParams, NodeGroupingRule} from './types';
 
 const {UNAUTHORIZED, DATA_SOURCE_UNAVAILABLE, ALREADY_EXISTS, NOT_FOUND} = LkErrorKey;
 
 export class NodeGroupingAPI extends Request {
-  public createNodeGroupingRule(params: CreateNodeGroupingRuleParams) {
+  public createNodeGroupingRule(
+    this: Request<NodeGroupingRule>,
+    params: CreateNodeGroupingRuleParams
+  ) {
     return this.request({
       errors: [UNAUTHORIZED, DATA_SOURCE_UNAVAILABLE, NOT_FOUND, ALREADY_EXISTS],
       url: '/:sourceKey/nodeGroupings',
