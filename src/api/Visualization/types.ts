4b6065669726fec1c4c6166a881f1c43c9c9dba3
mzorkany <mahmoud@linkurio.us>
2024-02-13 11:37:11 +0100

    LKE-10361 added nodeGroupingRuleId property

--- src/api/Visualization/types.ts
+++ src/api/Visualization/types.ts
@@ -170,6 +170,7 @@ export interface Visualization extends BaseVisualization, PersistedItem {
   nodes: IVizNodeInfo[];
   edges: IVizEdgeInfo[];
   nodeFields: IItemFields;
+  nodeGroupingRuleId: number | null;
   edgeFields: IItemFields;
   design: IVisualizationDesign;
   filters: IVisualizationFilters;
@@ -200,6 +201,7 @@ export interface ICreateVisualizationParams extends IDataSourceParams {
   title: string;
   folder?: number;
   nodes: IVizNodeInfo[];
+  nodeGroupingRuleId?: number | null;
   edges: IVizEdgeInfo[];
   alternativeIds?: IAlternativeIdSettings;
   mode?: VisualizationMode;
