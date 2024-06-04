d3b57800d14890ed9cbe5ec8facc68c0c49ef3e2
mzorkany <mahmoud@linkurio.us>
2024-06-04 14:57:55 +0200

    Update types.ts

--- src/api/Tag/types.ts
+++ src/api/Tag/types.ts
@@ -2,7 +2,7 @@
  * LINKURIOUS CONFIDENTIAL
  * Copyright Linkurious SAS 2012 - 2024
  *
- * - Created on 2024-06-4.
+ * - Created on 2024-06-04.
  */
 import {IDataSourceParams} from '../commonTypes';
 
