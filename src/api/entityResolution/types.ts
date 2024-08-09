fd5fd2be8809b9fc1ba81d51e6b9b5239d4c6dae
Guillaume Masclet <guillaume.masclet@linkurio.us>
2024-08-09 16:58:11 +0200

    LKE-11396: Post-review

--- src/api/entityResolution/types.ts
+++ src/api/entityResolution/types.ts
@@ -5,8 +5,26 @@
  * - Created on 2024-07-29.
  */
 
+/**
+ *  The status of entity resolution for a given data-source.
+ */
 export interface EntityResolutionStatus {
+  /**
+   * - `needed`: Entity resolution has never run or the last run failed.
+   * - `ongoing`: Entity resolution is currently running.
+   * - `done`: Entity resolution has successfully run.
+   */
   state: 'needed' | 'ongoing' | 'done';
+  /**
+   * The formatted progress percentage with two decimal digits (for instance `42.93`).
+   */
   progress: string;
+  /**
+   * A human readable message giving additionnal details.
+   */
   message: string;
+  /**
+   * The source-key of the data-source on which entity resolution is currently running, if any.
+   */
+  busySourceKey?: string;
 }
