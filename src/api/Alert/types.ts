3049f0eaf97b300bfb98aa26c61eff3ac5136d8f
mzorkany <mahmoud@linkurio.us>
2024-09-02 09:50:59 +0200

    LKE-11549 replace query property by name

--- src/api/Alert/types.ts
+++ src/api/Alert/types.ts
@@ -134,7 +134,7 @@ export interface IAlertQuery extends AlertQueryData {
 
 type AlertError = {
   queryId?: number;
-  query?: string;
+  name?: string;
   source: 'caseAttributeQuery' | 'alertQuery' | 'preprocessingStep';
   error: LkError;
   partial: boolean;
