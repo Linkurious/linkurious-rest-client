444a7ed73c604bb746a08d4e8cdad9d06f09bc5d
Guillaume Masclet <guillaume.masclet@linkurio.us>
2024-09-19 13:30:50 +0200

    LKE-12011: Add the entity resolution mapping API

--- src/api/entityResolution/index.ts
+++ src/api/entityResolution/index.ts
@@ -8,13 +8,77 @@ import {Request} from '../../http/request';
 import {LkErrorKey} from '../../http/response';
 import {IDataSourceParams} from '../commonTypes';
 
-import {EntityResolutionStatus} from './types';
+import {
+  CreateEntityResolutionMappingParams,
+  DeleteEntityResolutionMappingParams,
+  EntityResolutionMapping,
+  EntityResolutionStatus,
+  UpdateEntityResolutionMappingParams
+} from './types';
 
 export * from './types';
 
-const {UNAUTHORIZED, FORBIDDEN, DATA_SOURCE_UNAVAILABLE, ILLEGAL_SOURCE_STATE} = LkErrorKey;
+const {
+  ALREADY_EXISTS,
+  DATA_SOURCE_UNAVAILABLE,
+  FORBIDDEN,
+  ILLEGAL_SOURCE_STATE,
+  NOT_FOUND,
+  UNAUTHORIZED
+} = LkErrorKey;
 
 export class EntityResolutionAPI extends Request {
+  /**
+   * Create a new entity resolution mapping.
+   */
+  createEntityResolutionMapping(params: CreateEntityResolutionMappingParams) {
+    return this.request({
+      errors: [UNAUTHORIZED, FORBIDDEN, DATA_SOURCE_UNAVAILABLE, NOT_FOUND, ALREADY_EXISTS],
+      url: '/:sourceKey/entityResolution/mappings/:sourceNodeCategory',
+      method: 'POST',
+      params: params
+    });
+  }
+
+  /**
+   * Update an existing entity resolution mapping.
+   */
+  updateEntityResolutionMapping(params: UpdateEntityResolutionMappingParams) {
+    return this.request({
+      errors: [UNAUTHORIZED, FORBIDDEN, DATA_SOURCE_UNAVAILABLE, NOT_FOUND],
+      url: '/:sourceKey/entityResolution/mappings/:sourceNodeCategory',
+      method: 'PATCH',
+      params: params
+    });
+  }
+
+  /**
+   * Delete an existing entity resolution mapping.
+   */
+  deleteEntityResolutionMapping(params: DeleteEntityResolutionMappingParams) {
+    return this.request({
+      errors: [UNAUTHORIZED, FORBIDDEN, DATA_SOURCE_UNAVAILABLE, NOT_FOUND],
+      url: '/:sourceKey/entityResolution/mappings/:sourceNodeCategory',
+      method: 'DELETE',
+      params: params
+    });
+  }
+
+  /**
+   * List all the entity resolution mappings for a given data-source.
+   */
+  getEntityResolutionMappings(
+    this: Request<{items: EntityResolutionMapping[]}>,
+    params: IDataSourceParams
+  ) {
+    return this.request({
+      errors: [UNAUTHORIZED, FORBIDDEN, DATA_SOURCE_UNAVAILABLE],
+      url: '/:sourceKey/entityResolution/mappings',
+      method: 'GET',
+      params: params
+    });
+  }
+
   /**
    * Start entity resolution on a given data-source.
    */
--- src/api/entityResolution/types.ts
+++ src/api/entityResolution/types.ts
@@ -4,13 +4,146 @@
  *
  * - Created on 2024-07-29.
  */
+import {IDataSourceParams} from '../commonTypes';
+
+export type CreateEntityResolutionMappingParams = IDataSourceParams & EntityResolutionMapping;
+export type UpdateEntityResolutionMappingParams = CreateEntityResolutionMappingParams;
+export type DeleteEntityResolutionMappingParams = IDataSourceParams & {sourceNodeCategory: string};
+
+/**
+ * Describe how to convert a graph node into a entity resolution record.
+ */
+export interface EntityResolutionMapping<
+  T extends EntityResolutionRecordType = EntityResolutionRecordType
+> {
+  sourceNodeCategory: string;
+  targetRecordType: T;
+  properties: EntityResolutionPropertyMapping<T>[];
+}
+
+export interface EntityResolutionPropertyMapping<
+  T extends EntityResolutionRecordType = EntityResolutionRecordType
+> {
+  sourcePropertyKey: string;
+  targetAttribute: RecordAttribute<T>;
+}
+
+export type RecordAttribute<T extends EntityResolutionRecordType = EntityResolutionRecordType> =
+  | RecordSimpleAttribute<T>
+  | RecordComplexAttribute<T>;
+
+export type RecordSimpleAttribute<
+  T extends EntityResolutionRecordType = EntityResolutionRecordType
+> =
+  | CommonSimpleAttribute
+  | (T extends 'person'
+      ? PersonSimpleAttribute
+      : T extends 'organization'
+        ? OrganizationSimpleAttribute
+        : never);
+
+export type RecordComplexAttribute<
+  T extends EntityResolutionRecordType = EntityResolutionRecordType
+> =
+  | ComplexAttribute<'addresses', RecordAddressAttribute>
+  | ComplexAttribute<'phones', RecordPhoneAttribute>
+  | (T extends 'person'
+      ? ComplexAttribute<'names', PersonNameAttribute>
+      : T extends 'organization'
+        ? ComplexAttribute<'names', OrganizationNameAttribute>
+        : never);
+
+type ComplexAttribute<Parent, Child> = [Parent, type: string, Exclude<Child, 'type'>];
 
 /**
  * The supported record types.
  */
 export const ENTITY_RESOLUTION_RECORD_TYPES = ['person', 'organization'] as const;
-
 export type EntityResolutionRecordType = (typeof ENTITY_RESOLUTION_RECORD_TYPES)[number];
+
+export const COMMON_SIMPLE_ATTRIBUTES = [
+  'accountNumber',
+  'accountDomain',
+  'websiteAddress',
+  'emailAddress',
+  'linkedin',
+  'facebook',
+  'twitter',
+  'skype',
+  'zoomRoom',
+  'instagram',
+  'whatsapp',
+  'signal',
+  'telegram',
+  'tango',
+  'viber',
+  'wechat'
+] as const;
+export type CommonSimpleAttribute = (typeof COMMON_SIMPLE_ATTRIBUTES)[number];
+
+export const PERSON_SIMPLE_ATTRIBUTES = [
+  'gender',
+  'dateOfBirth',
+  'dateOfDeath',
+  'nationality',
+  'citizenship',
+  'placeOfBirth',
+  'passportNumber',
+  'passportCountry',
+  'driverLicenseNumber',
+  'driverLicenseState',
+  'ssnNumber',
+  'nationalIdNumber',
+  'nationalIdCountry'
+] as const;
+export type PersonSimpleAttribute = (typeof PERSON_SIMPLE_ATTRIBUTES)[number];
+
+export const ORGANIZATION_SIMPLE_ATTRIBUTES = [
+  'registrationDate',
+  'registrationCountry',
+  'taxIdType',
+  'taxIdNumber',
+  'taxIdCountry',
+  'dunsNumber',
+  'npiNumber',
+  'leiNumber'
+] as const;
+export type OrganizationSimpleAttribute = (typeof ORGANIZATION_SIMPLE_ATTRIBUTES)[number];
+
+export const ORGANIZATION_NAME_ATTRIBUTES = ['type', 'org'] as const;
+export type OrganizationNameAttribute = (typeof ORGANIZATION_NAME_ATTRIBUTES)[number];
+
+export const RECORD_ADDRESS_ATTRIBUTES = [
+  'type',
+  'full',
+  'line1',
+  'line2',
+  'line3',
+  'line4',
+  'line5',
+  'line6',
+  'city',
+  'state',
+  'postalCode',
+  'country',
+  'fromDate',
+  'thruDate'
+] as const;
+export type RecordAddressAttribute = (typeof RECORD_ADDRESS_ATTRIBUTES)[number];
+
+export const RECORD_PHONE_ATTRIBUTES = ['type', 'number', 'fromDate', 'thruDate'] as const;
+export type RecordPhoneAttribute = (typeof RECORD_PHONE_ATTRIBUTES)[number];
+
+export const PERSON_NAME_ATTRIBUTES = [
+  'type',
+  'full',
+  'first',
+  'middle',
+  'last',
+  'prefix',
+  'suffix'
+] as const;
+export type PersonNameAttribute = (typeof PERSON_NAME_ATTRIBUTES)[number];
 
 /**
  *  The status of entity resolution for a given data-source.
