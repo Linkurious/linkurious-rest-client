ff769165708723e2b1868df607671ac3ac37b753
mzorkany <mahmoud@linkurio.us>
2024-10-01 18:10:53 +0200

    Update types.ts

--- src/api/Alert/types.ts
+++ src/api/Alert/types.ts
@@ -116,7 +116,7 @@ export interface Alert extends IBaseAlert, PersistedItem {
   sourceKey: string;
   lastRun?: string; // defined if it has run at least once
   // defined if last run had a problem
-  lastRunProblem?: AlertError[];
+  lastRunProblem?: AlertErrorType[];
   nextRun?: string; // defined if enabled=true
   openAndUnAssignedCasesCount: number;
   status: 'running' | 'idle';
