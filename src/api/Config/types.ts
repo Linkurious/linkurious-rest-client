d5dea7a5a67f7903eff645a8e3cf3bb11fdb4279
mzorkany <mahmoud@linkurio.us>
2024-10-29 13:43:00 +0100

    LKE-12219 post review

--- src/api/Config/types.ts
+++ src/api/Config/types.ts
@@ -249,7 +249,7 @@ export interface IAdvancedConfig {
   itemTypeCountLimit?: number;
   dataSourceConnectionTimeout?: number;
   dataSourceAutoReconnectInterval?: number;
-  flags?: string[];
+  flags?: GenericObject;
 }
 
 export interface ILeafletConfig {
