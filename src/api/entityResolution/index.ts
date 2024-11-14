19144f9aab591a7f0e89f1f46da0819cc452ba3d
Guillaume Masclet <guillaume.masclet@linkurio.us>
2024-11-14 16:33:36 +0100

    LKE-11926: Finer grained entity resolution ingestion (#764)

--- src/api/entityResolution/index.ts
+++ src/api/entityResolution/index.ts
@@ -13,7 +13,7 @@ import {
   DeleteEntityResolutionMappingParams,
   EntityResolutionMapping,
   IngestionStatus,
-  StartIngestionParams,
+  StartEntityResolutionTaskParams,
   UpdateEntityResolutionMappingParams
 } from './types';
 
@@ -30,7 +30,7 @@ const {
 
 export class EntityResolutionAPI extends Request {
   /**
-   * Create a new entity resolution mapping.
+   * Create a new entity resolution mapping, for a given node category.
    */
   createEntityResolutionMapping(params: CreateEntityResolutionMappingParams) {
     return this.request({
@@ -42,7 +42,7 @@ export class EntityResolutionAPI extends Request {
   }
 
   /**
-   * Update an existing entity resolution mapping.
+   * Update an existing entity resolution mapping, for a given node category.
    */
   updateEntityResolutionMapping(params: UpdateEntityResolutionMappingParams) {
     return this.request({
@@ -54,7 +54,7 @@ export class EntityResolutionAPI extends Request {
   }
 
   /**
-   * Delete an existing entity resolution mapping.
+   * Delete an existing entity resolution mapping, for a given node category.
    */
   deleteEntityResolutionMapping(params: DeleteEntityResolutionMappingParams) {
     return this.request({
@@ -81,13 +81,57 @@ export class EntityResolutionAPI extends Request {
   }
 
   /**
-   * Start the entity resolution ingestion on a given data-source.
+   * Start a full ingestion task on a given data-source. This task:
+   * - Ensures all the graph indexes needed for entity resolution are created.
+   * - Fetches all the graph nodes for each mapped category.
+   * - Converts each fetched node into an entity resolution record.
+   * - Sends all the converted records to the entity resolution server.
+   * - Materializes the resolved entities in the graph database.
+   *
+   * By default, immediately returns, without waiting for the task to complete.
    */
-  startIngestion(params: StartIngestionParams) {
+  startFullIngestion(params: StartEntityResolutionTaskParams) {
     return this.request({
       errors: [UNAUTHORIZED, FORBIDDEN, DATA_SOURCE_UNAVAILABLE, ILLEGAL_SOURCE_STATE],
       url: '/:sourceKey/entityResolution',
       method: 'POST',
+      params: params
+    });
+  }
+
+  /**
+   * Start an incremental ingestion task on a given data-source. This task is similar to a full
+   * ingestion task, but it only fetches the graph nodes that have been modified after the latest
+   * ingestion.
+   *
+   * This task is going to fail if:
+   * - Incremental ingestion is not configured for the data-source.
+   * - Or if the ingestion state is not `done` (a full ingestion has to be done first).
+   *
+   * By default, immediately returns, without waiting for the task to complete.
+   */
+  startIncrementalIngestion(params: StartEntityResolutionTaskParams) {
+    return this.request({
+      errors: [UNAUTHORIZED, FORBIDDEN, DATA_SOURCE_UNAVAILABLE, ILLEGAL_SOURCE_STATE],
+      url: '/:sourceKey/entityResolution',
+      method: 'PATCH',
+      params: params
+    });
+  }
+
+  /**
+   * Start a purge task for a given data-source. This task:
+   * - Removes all the entity nodes/edges in the graph database.
+   * - Deletes all the graph indexes related to entity resolution.
+   * - Empties the data-source in the entity-resolution server.
+   *
+   * By default, immediately returns, without waiting for the task to complete.
+   */
+  startPurge(params: StartEntityResolutionTaskParams) {
+    return this.request({
+      errors: [UNAUTHORIZED, FORBIDDEN, DATA_SOURCE_UNAVAILABLE, ILLEGAL_SOURCE_STATE],
+      url: '/:sourceKey/entityResolution',
+      method: 'DELETE',
       params: params
     });
   }
