fb3e52e2b702d2b57eed38ffec7af7949bbf38c7
Hussein Bassam <husseinbassam95@gmail.com>
2025-06-03 15:40:07 +0200

    LKE-13380 feat(ER): Add ER source key config

--- src/api/Config/types.ts
+++ src/api/Config/types.ts
@@ -50,6 +50,7 @@ export interface Configuration {
   needRestart?: boolean;
   emailNotifications: IEmailNotificationsConfig;
   entityResolution?: EntityResolutionConfig;
+  entityResolutionSourceKey?: string;
   cluster?: ClusterConfig;
 }
 
