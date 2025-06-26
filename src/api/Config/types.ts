737fd91d76cf9f2c65e22578856f4a8cdb59ec2a
Guillaume Masclet <guillaume.masclet@linkurio.us>
2025-06-26 15:54:32 +0200

    LKE-13497: Disable custom groups

--- src/api/Config/types.ts
+++ src/api/Config/types.ts
@@ -276,6 +276,7 @@ export interface IAccessConfig {
   floatingLicenses?: number;
   authRequired?: boolean;
   disableLocalAuth?: boolean;
+  enableCustomGroups?: boolean;
   guestMode?: boolean;
   dataEdition?: boolean;
   widget?: boolean;
